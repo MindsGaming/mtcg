6fe0f6ff9247e9d8fad5f503ab11d048ba42dd33
Glitch (hello-sqlite) <none>
2021-10-16 02:11:09 +0000

    🌖📌 Checkpoint ./views/index.html:5646401/1 ./public/js/mice.js:5646401/7361

--- public/js/mice.js
+++ public/js/mice.js
@@ -15,113 +15,26 @@ function cashTokens() {
   const minutesLabel = document.getElementById("minutes");
   const secondsLabel = document.getElementById("seconds");
 
-  const dreamTokens = minutesLabel.innerHTML + secondsLabel.innerHTML;
-
-  function gamertokens() {
-    var dreamtokens = document.getElementById("gamertokens");
-    dreamtokens.classList.toggle("dreamtokens");
-    dreamInputplayer.value = "GAMER";
-  }
-
-  function doobettertokens() {
-    var dreamtokens = document.getElementById("doobettertokens");
-    dreamtokens.classList.toggle("dreamtokens");
-    dreamInputplayer.value = "DooBetter";
-  }
-
-  // request the dreams from our app's sqlite database
-  fetch("/getDreams", {})
-    .then(res => res.json())
-    .then(response => {
-      response.forEach(row => {
-        appendNewDream(row.dream);
-      });
-    });
-
-  // a helper function that creates a list item for a given dream
-  const appendNewDream = dream => {
-    const newListItem = document.createElement("li");
-    newListItem.innerText = dream;
-    dreamsList.appendChild(newListItem);
+  const dreamTokens = minutesLabel.innerHTML + "." + secondsLabel.innerHTML;
+  const dreamedTokens = "Rewards: " + dreamTokens;
+const data = {
+    dream:
+      dreamInput.value + "-" + dreamInputuser.value + dreamInputplayer.value + dreamedTokens
   };
-
-  // listen for the form to be submitted and add a new dream when it is
-  dreamsForm.onsubmit = event => {
-    // stop our form submission from refreshing the page
-    event.preventDefault();
-
-    const data = {
-      dream:
-        dreamInput.value + "-" + dreamInputuser.value + dreamInputplayer.value
-    };
-
-    fetch("/addDream", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" }
-    })
-      .then(res => res.json())
-      .then(response => {
-        console.log(JSON.stringify(response));
-      });
-    // get dream value and add it to the list
-    dreams.push(
-      dreamInput.value +
-        "-" +
-        dreamInputuser.value +
-        dreamInputplayer.value +
-        dreamTokens
-    );
-    appendNewDream(
-      dreamInput.value +
-        "-" +
-        dreamInputuser.value +
-        dreamInputplayer.value +
-        dreamTokens
-    );
-
-    var txt;
-    var r = confirm("Select A Miner!");
-    if (r == true) {
-      var badge = document.getElementById("badge");
-      badge.innerHTML = dreamInputuser.value;
-      badge.title = dreamInputuser.value;
-
-      var login = document.getElementById("login");
-      login.classList.toggle("hide");
-      var backgrounds = document.getElementById("backgrounds");
-      backgrounds.classList.toggle("hide");
-    } else {
-      txt = "Humm okkay";
-    }
-
-    // reset form
-    dreamInput.focus();
-  };
-
-  clearButton.addEventListener("click", event => {
-    fetch("/clearDreams", {})
-      .then(res => res.json())
-      .then(response => {
-        console.log("cleared dreams");
-      });
-    dreamsList.innerHTML = "";
-  });
-
-  function guest() {
-    var txt;
-    var r = confirm("Login To Earn Rewards!");
-    if (r == true) {
-      var badge = document.getElementById("badge");
-      badge.innerHTML = "Guest";
-      badge.title = "Guest";
-
-      var login = document.getElementById("login");
-      login.classList.toggle("hide");
-      var backgrounds = document.getElementById("backgrounds");
-      backgrounds.classList.toggle("hide");
-    } else {
-      txt = "Humm okkay";
-    }
-  }
+  dreams.push(
+    dreamInput.value +
+      "-" +
+      dreamInputuser.value +
+      dreamInputplayer.value +
+      dreamedTokens +
+      "Check"
+  );
+  appendNewDream(
+    dreamInput.value +
+      "-" +
+      dreamInputuser.value +
+      dreamInputplayer.value +
+      dreamedTokens +
+      "check"
+  );
 }
