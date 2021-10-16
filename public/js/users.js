a74e7ff88a6e0b840cc452b3340308fafd839f2a
Glitch (hello-sqlite) <none>
2021-10-16 05:22:14 +0000

    🍉👫 Checkpoint ./public/js/users.js:5646401/1487 ./views/index.html:5646401/5 ./public/fly.css:5646401/54

---
+++ public/js/users.js
@@ -0,0 +1,50 @@
+// client-side js
+// run by the browser each time your view template referencing it is loaded
+function cashTokens() {
+  const dreams = [];
+
+  // define variables that reference elements on our page
+  const dreamsForm = document.forms[0];
+  const dreamInput = dreamsForm.elements["dream"];
+  const dreamInputuser = dreamsForm.elements["username"];
+  const dreamInputplayer = dreamsForm.elements["tokens"];
+
+  const dreamsList = document.getElementById("dreams");
+  const clearButton = document.querySelector("#clear-dreams");
+
+  const minutesLabel = document.getElementById("minutes");
+  const secondsLabel = document.getElementById("seconds");
+  const tokenCheck = document.getElementById("tokenCheck");
+
+  const tokenChecking = minutesLabel.innerHTML + "." + secondsLabel.innerHTML;
+  tokenCheck.value = tokenChecking;
+
+  if (minutesLabel.innerHTML == "00") {
+    alert("You Need A Full Token TO Cash Out!");
+  } else {
+    var txt;
+    var r = confirm("Are You Sure You Want To Claim & Logout?");
+    if (r == true) {
+      const data = {
+        dream:
+          dreamInput.value +
+          "-" +
+          dreamInputuser.value +
+          dreamInputplayer.value +
+          " Reward: "
+      };
+
+      appendNewDream(
+        dreamInput.value +
+          "-" +
+          dreamInputuser.value +
+          " Reward: " +
+          dreamInputplayer.value +
+          tokenCheck.value
+      );
+      window.open("/", "_self");
+    } else {
+      txt = "You pressed Cancel!";
+    }
+  }
+}
