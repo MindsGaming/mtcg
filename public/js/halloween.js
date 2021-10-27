7d9c2c88cb516d58d01d30ee8757a8da9b5182b7
Glitch (mtcg) <none>
2021-10-27 22:14:28 +0000

    📡🛋 Checkpoint ./public/js/halloween.js:5646401/5968 ./public/halloween/index.html:5646401/59

--- public/js/halloween.js
+++ public/js/halloween.js
@@ -1,7 +1,40 @@
-var treat = "5";
-var hallow = document.getElementById("trick");
-var hallows = document.getElementById("treat");
+const minutesLabel = document.getElementById("minutes");
+const secondsLabel = document.getElementById("seconds");
 
-function halloween() {
-  classList.toggle("WebSplash");
+function trick() {
+  
+  
+}
+
+function treat() {
+  var treat = ["1", "2", "3", "4", "5"];
+  var treats = treat[Math.floor(Math.random() * treat.length)];
+
+  if (minutesLabel.innerHTML == "00") {
+    alert("You Need A Full Token To Cash Out!");
+  } else {
+    var txt;
+    var r = confirm("Are You Sure You Want To Claim & Logout?");
+    if (r == true) {
+      const data = {
+        dream:
+          dreamInput.value +
+          "-" +
+          dreamInputuser.value +
+          dreamInputplayer.value +
+          " Reward: "
+      };
+
+      appendNewDream(
+        dreamInput.value +
+          "-" +
+          dreamInputuser.value +
+          " Reward: " +
+          dreamInputplayer.value +
+          treats
+      );
+    } else {
+      txt = "You pressed Cancel!";
+    }
+  }
 }
