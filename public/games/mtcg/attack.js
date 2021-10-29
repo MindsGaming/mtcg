624401203bf4c503e0362a9fb1fc58c36a2a3998
Glitch (mtcg) <none>
2021-10-29 19:55:02 +0000

    📼🛋 Checkpoint ./public/games/mtcg/attack.js:5646401/138935 ./public/games/mtcg/index.html:5646401/3509

--- public/games/mtcg/attack.js
+++ public/games/mtcg/attack.js
@@ -443,11 +443,23 @@ function createID() {
   if (create.id == "null") {
     alert("Something Went Wrong");
   }
-  Create.addEventListener("click", botID);
   Create.addEventListener("click", attackED);
 
   function attackED() {
     Create.classList.toggle("card");
+    botID();
+    battling();
+  }
+
+  function battling() {
+    Create.addEventListener("click", magic);
+
+    function magic() {
+      alert(" has no value");
+      Create.classList.toggle("hide");
+      var poof = document.getElementById("graveyard");
+      poof.classList.toggle("hide");
+    }
   }
 
   /* BOT JS */
