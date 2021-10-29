62af7f071f9ab12a3fdbff566c50a45a8538cb91
Glitch (mtcg) <none>
2021-10-29 15:45:37 +0000

    👲❄️ Checkpoint ./public/games/mtcg/listen.js:5646401/465 ./public/games/mtcg/attack.js:5646401/68398 ./public/games/mtcg/bot.js:5646401/90665 ./public/games/mtcg/index.html:5646401/3277

---
+++ public/games/mtcg/bot.js
@@ -0,0 +1,9 @@
+/* PlayDeck */
+const playdeck = document.createElement("img");
+playdeck.src =
+  "https://lh3.googleusercontent.com/wXCSrw63MDFpFOYtqFHaIyKwYaQbBU4dMln5PZKKpcmQajwbiHMPGRepko1FSzeGbbButgpi8PNiTppf5eWm3KHsVfuFEP9D-q71WPg=w301";
+playdeck.style = "position:fixed; bottom: 0;right:0; width:120px";
+playdeck.title = "PlayDeck";
+playdeck.id = "PlayedDeck";
+var PlayDeck = document.getElementById("playdeck").appendChild(playdeck);
+
--- public/games/mtcg/listen.js
+++
@@ -1,7 +0,0 @@
-const MTCGmove = document.getElementById("mtcg");
-
-MTCGmove.addEventListener("click", attackED);
-
-function attackED() {
-  MTCGmove.classList.toggle("card");
-}
