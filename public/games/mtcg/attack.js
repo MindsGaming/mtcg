42a23d9e53feb5ebfe43c3e235fd1636582aed9d
Glitch (mtcg) <none>
2021-10-30 23:23:13 +0000

    👚🛬 Checkpoint ./public/games/mtcg/index.html:5646401/15412 ./public/games/mtcg/attack.js:5646401/35219

--- public/games/mtcg/attack.js
+++ public/games/mtcg/attack.js
@@ -2,7 +2,7 @@
 var playdeck = document.createElement("img");
 playdeck.src =
   "https://lh3.googleusercontent.com/wXCSrw63MDFpFOYtqFHaIyKwYaQbBU4dMln5PZKKpcmQajwbiHMPGRepko1FSzeGbbButgpi8PNiTppf5eWm3KHsVfuFEP9D-q71WPg=w301";
-playdeck.style = "position:fixed; bottom: 0;right:0; width:120px";
+playdeck.width = "120";
 playdeck.title = "PlayDeck";
 playdeck.id = "PlayedDeck";
 var PlayDeck = document.getElementById("playdeck").appendChild(playdeck);
@@ -451,7 +451,6 @@ function createID() {
     botID();
   }
 
- 
   function battling() {
     Create.addEventListener("click", magic);
 
