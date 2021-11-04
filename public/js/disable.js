56eba32d52d66cd5d103ff95c34ec33daf034829
Glitch (mtcg) <none>
2021-11-04 20:29:37 +0000

    🍂🎸 Checkpoint ./public/js/disable.js:5646401/1126 ./views/index.html:5646401/30

--- public/js/disable.js
+++ public/js/disable.js
@@ -4,6 +4,13 @@ function cashTokens() {
   alert("Cashout is currently disabled. Try Again Later");
 }
 
-
 const disable = document.getElementById("submit-dream");
-    Create.addEventListener("click", magic);
+disable.addEventListener("click", cleaning);
+
+function cleaning() {
+  alert("Claims Are Being Processed Try Again Later");
+}
+
+function guest() {
+  alert("Claims Are Being Processed Try Again Later");
+}
