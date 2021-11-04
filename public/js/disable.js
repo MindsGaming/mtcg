d0008ca9266448e6e0de04013a631f12a85d3923
Glitch (mtcg) <none>
2021-11-04 20:17:38 +0000

    🐗👟 Checkpoint ./public/js/disable.js:5646401/116

--- public/js/disable.js
+++ public/js/disable.js
@@ -3,3 +3,7 @@
 function cashTokens() {
   alert("Cashout is currently disabled. Try Again Later");
 }
+
+
+const disable = document.getElementById("submit-dream");
+    Create.addEventListener("click", magic);
