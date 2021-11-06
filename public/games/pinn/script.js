158ea6393c8b50a426dd5b601e3588e16b415604
Glitch (mtcg) <none>
2021-11-06 19:47:04 +0000

    🚶🐀 Checkpoint

--- public/games/pinn/script.js
+++
@@ -1,9 +0,0 @@
-const LIFE = ["1", "2", "3", "4", "5"];
-
-var life = document.getElementByID("1");
-life.addEventListener("click", createLife);
-
-function createLife() {
-  var tools = document.getElementById(LIFE);
-  tools.classList.toggle("life");
-}
---
+++ public/games/pinn/wonder.js
@@ -0,0 +1,82 @@
+function startGamed() {
+  var start = document.getElementById("startGame");
+  start.innerHTML = "Countiue Universe";
+  start.style = "background-color: blue; color: gold;";
+
+  var cashout = document.getElementById("cashOut");
+  cashout.style =
+    "position: fixed; display: block; right: 0; top: 0;  width: 50px; background-color: white; border-style: groove; border-color: gold; padding: 1px; margin: 25px; color: red; cursor:pointer;";
+
+  var spoof = ["1", "2", "3", "4", 1.7976931348623157e10308];
+  const random = spoof[Math.floor(Math.random() * spoof.length)];
+  var ID = random;
+
+  var lifechecks = document.getElementById(ID);
+  var lifecheck = ["life", "block", "earth"];
+  const lifeCheck = lifecheck[Math.floor(Math.random() * lifecheck.length)];
+
+  var life = document.createElement("div");
+  life.className = lifeCheck;
+  life.id = ID;
+  var LIFE = document.getElementById("gameoflife").appendChild(life);
+
+  if (life.id == "Infinity") {
+    life.title = "LIFE";
+    createLife();
+    startGamed();
+  }
+
+  if ((life.id == "2", "3", "4", "Infinity")) {
+    var spoofNew = ["1", "2", "3", "4", 1.7976931348623157e10308];
+    const randomNew = spoofNew[Math.floor(Math.random() * spoofNew.length)];
+    var IDnew = randomNew;
+
+    var lifechecksNew = document.getElementById(ID);
+    var lifecheckNew = ["life", "block", "earth"];
+    const lifeCheckNew =
+      lifecheckNew[Math.floor(Math.random() * lifecheckNew.length)];
+
+    var lifeNew = document.createElement("div");
+    lifeNew.className = lifeCheckNew;
+    lifeNew.id = IDnew;
+    var LIFENEW = document.getElementById("gameoflife").appendChild(lifeNew);
+
+    if (lifeNew.id == "Infinity") {
+      lifeNew.name = "Intelagent";
+      lifeNew.className = "Token";
+      startGamed();
+    }
+    createLife();
+  }
+
+  function createLife() {
+    var spoofNew = ["1", "2", "3", "4", 1.7976931348623157e10308];
+    const randomNew = spoofNew[Math.floor(Math.random() * spoofNew.length)];
+    var IDnew = randomNew;
+
+    var lifechecksNew = document.getElementById(ID);
+    var lifecheckNew = ["life", "block", "earth"];
+    const lifeCheckNew =
+      lifecheckNew[Math.floor(Math.random() * lifecheckNew.length)];
+
+    var lifeNew = document.createElement("div");
+    lifeNew.className = lifeCheckNew;
+    lifeNew.id = IDnew;
+    var LIFENEW = document.getElementById("gameoflife").appendChild(lifeNew);
+
+    if ((lifeNew.id == "2", "3", "4", "Infinity")) {
+      startGamed();
+    }
+  }
+}
+
+function cashOut() {
+  var intelagent = document.getElementById("Infinity");
+
+  if (intelagent.name == "Intelagent") {
+    alert("You Made Intelagent Life");
+    window.open("/", "_self");
+  } else {
+    alert("No Intelagent Life");
+  }
+}
