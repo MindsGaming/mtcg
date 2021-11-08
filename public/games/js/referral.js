439c4f2e837abad83b74ada14b08238e9e43cc35
Glitch (mtcg) <none>
2021-11-08 21:17:49 +0000

    🚦🙌 Checkpoint ./public/beta/index.html:5646401/79212 ./public/fly.css:5646401/19 ./admin/referral/index.html:5646401/334 ./public/games/js/referral.js:5646401/687

---
+++ public/games/js/referral.js
@@ -0,0 +1,12 @@
+const referral = document.getElementById("referral");
+
+const txt = "Referral";
+function playGamer() {
+  dreamInputplayer.value == "GAMER";
+  window.open("/games/gamer", "_self");
+}
+
+function playDoobetter() {
+  dreamInputplayer.value == "DooBetter";
+  window.open("/games/doobetter", "_blank");
+}
