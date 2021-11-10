6956cd1bf5cb2ce0edb06d9788a7753f439739f6
Glitch (mtcg) <none>
2021-11-10 01:34:51 +0000

    👫🐋 Checkpoint ./public/games/pinn/pinn.js:5646401/1849 ./public/games/pinn/beta/index.html:5646401/5768

--- public/games/pinn/pinn.js
+++ public/games/pinn/pinn.js
@@ -1,20 +1,12 @@
 function createPinn() {
   var tack = document.createElement("div");
   tack.className = "tack";
-  tack.id = "tack" + 1;
+  tack.id = "PINN";
   var TACK = document.getElementById("tack").appendChild(tack);
 
   var tackCount = document.getElementById("tackCount").value;
   var tackcount = tackCount + 1;
   document.getElementById("tackCount").value = tackcount;
 
-   document.getElementById("total").innerHTML = tackcount;
-  
-  var countrewards = document.getElementByID("total");
-  
-  if (countrewards.innerHTML = "100"){
-    var life = document.getElementByID("life");
-    
-  }
-
+  document.getElementById("total").innerHTML = tackCount;
 }
