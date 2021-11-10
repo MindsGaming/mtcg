cc4aba429c6e132aa11e9c1d5fecd14d1afbc42c
Glitch (mtcg) <none>
2021-11-10 03:08:12 +0000

    ⏲🐗 Checkpoint ./public/games/pinn/beta/index.html:5646401/7404 ./public/games/pinn/pinn.js:5646401/4109

--- public/games/pinn/pinn.js
+++ public/games/pinn/pinn.js
@@ -22,3 +22,36 @@ function createPinn() {
     document.getElementById("tackCount").value = "1";
   }
 }
+
+function cashOut() {
+  var rewards = document.getElementById("rewards");
+  var total = document.getElementById("total");
+
+  if (rewards.innerHTML == "0") {
+    alert("You Need A Full Token TO Cash Out!");
+  } else {
+    var txt;
+    var r = confirm("Quit Already?");
+    if (r == true) {
+      const data = {
+        dream:
+          dreamInput.value +
+          "-" +
+          dreamInputuser.value +
+          dreamInputplayer.value +
+          " Reward: "
+      };
+
+      appendNewDream(
+        dreamInput.value +
+          "-" +
+          dreamInputuser.value +
+          " Reward: " +
+          dreamInputplayer.value +
+          rewards.value +
+          total.innerHTML
+      );
+      window.open("./");
+    } else txt = "You pressed Cancel!";
+  }
+}
