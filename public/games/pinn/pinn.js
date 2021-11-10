5b178e8f08d6337a3a2350a268f7428b39b52aa8
Glitch (mtcg) <none>
2021-11-10 01:22:51 +0000

    🍔🚄 Checkpoint ./public/games/pinn/pinn.js:5646401/197

--- public/games/pinn/pinn.js
+++ public/games/pinn/pinn.js
@@ -2,12 +2,19 @@ function createPinn() {
   var tack = document.createElement("div");
   tack.className = "tack";
   tack.id = "tack" + 1;
-  var LIFE = document.getElementById("tack").appendChild(tack);
+  var TACK = document.getElementById("tack").appendChild(tack);
 
   var tackCount = document.getElementById("tackCount").value;
   var tackcount = tackCount + 1;
   document.getElementById("tackCount").value = tackcount;
 
    document.getElementById("total").innerHTML = tackcount;
+  
+  var countrewards = document.getElementByID("total");
+  
+  if (countrewards.innerHTML = "100"){
+    var life = document.getElementByID("life");
+    
+  }
 
 }
