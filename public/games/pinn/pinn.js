cd10d545a3672b6147e32dfb9ba16782eec3fc3b
Glitch (mtcg) <none>
2021-11-10 00:58:52 +0000

    🌴🐮 Checkpoint ./public/games/pinn/pinn.js:5646401/933 ./public/games/pinn/beta/index.html:5646401/37436

--- public/games/pinn/pinn.js
+++ public/games/pinn/pinn.js
@@ -1,7 +1,7 @@
 function createPinn() {
   var tack = document.createElement("div");
   tack.className = "tack";
-  tack.id = "tack" +1;
+  tack.id = "tack" + 1;
   var LIFE = document.getElementById("tack").appendChild(tack);
 
   var tackCount = document.getElementById("tackCount").value;
