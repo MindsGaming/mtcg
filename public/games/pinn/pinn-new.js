485ee133584314761248ff1a16e5ba64a0b20d44
Glitch (mtcg) <none>
2021-11-16 19:22:02 +0000

    🚉🏎 Checkpoint ./public/games/pinn/beta/index.html:5646401/24523 ./public/games/pinn/pinn-new.js:5646401/1037

--- public/games/pinn/pinn-new.js
+++ public/games/pinn/pinn-new.js
@@ -1,27 +1,10 @@
-function createPinn() {
-  var tack = document.createElement("div");
-  tack.className = "tack";
-  tack.id = "PINN";
-  var TACK = document.getElementById("tack").appendChild(tack);
+const makefood = document.getElementByID("makefood");
+const makeFood = document.getElementByID("makeFood");
+const makefood = document.getElementByID("makefood");
 
-  var tackCount = document.getElementById("tackCount").value;
-  var tackcount = tackCount + 1;
-  document.getElementById("tackCount").value = tackcount;
 
-  document.getElementById("total").innerHTML = tackCount;
 
-  var addReward = document.getElementById("total");
 
-  if (addReward.innerHTML == "100") {
-    var rewards = document.getElementById("rewards").value;
-    var rewardscount = rewards + 1;
-    document.getElementById("rewards").value = rewardscount;
-    var rewardstotal = document.getElementById("rewardstotal");
-    rewardstotal.innerHTML = rewardscount;
-    document.getElementById("total").innerHTML = "00";
-    document.getElementById("tackCount").value = "1";
-  }
-}
 
 function cashOut() {
   var rewards = document.getElementById("rewards");
