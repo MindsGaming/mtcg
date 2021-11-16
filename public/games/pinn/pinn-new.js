1802d75ab061ba6b48f00e734f2232df43ad8638
Glitch (mtcg) <none>
2021-11-16 19:59:00 +0000

    📹🚛 Checkpoint ./public/games/pinn/pinn-new.js:5646401/6751 ./public/games/pinn/beta/index.html:5646401/22717

--- public/games/pinn/pinn-new.js
+++ public/games/pinn/pinn-new.js
@@ -7,7 +7,30 @@ const servedFood = document.getElementById("servedFood");
 const rewards = document.getElementById("rewards");
 const Rewards = document.getElementById("Rewards");
 
-function foodImage(){
-  
-  
+function foodImage() {
+  var tack = document.createElement("div");
+  tack.className = "makefood";
+  tack.id = "Made Food";
+  var TACK = document.getElementById("foodImage").appendChild(tack);
+
+  var tackCount = document.getElementById("makeFood").value;
+  var tackcount = tackCount + 1;
+  document.getElementById("makeFood").value = tackcount;
+
+  document.getElementById("makefood").innerHTML = tackCount;
+
+  var addReward = document.getElementById("makefood");
+
+  if (addReward.innerHTML == "100") {
+    var rewards = document.getElementById("platedFood").value;
+    var rewardscount = rewards + 1;
+    document.getElementById("PlatedFood").value = rewardscount;
+    var rewardstotal = document.getElementById("platedfood");
+    rewardstotal.innerHTML = rewardscount;
+    document.getElementById("makefood").innerHTML = "00";
+    document.getElementById("makeFood").value = "1";
+
+    var plateButton = document.getElementById("plateButton");
+    plateButton.classList.toggle("buttonReady");
+  }
 }
