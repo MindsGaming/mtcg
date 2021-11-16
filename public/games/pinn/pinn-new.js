c5d5a9e9c03940e5ae2181d3c868c6fcb3b54d11
Glitch (mtcg) <none>
2021-11-16 20:34:15 +0000

    📒🕋 Checkpoint ./public/games/pinn/pinn-new.js:5646401/889

--- public/games/pinn/pinn-new.js
+++ public/games/pinn/pinn-new.js
@@ -27,5 +27,30 @@ function foodImage() {
 
     var plateButton = document.getElementById("plateButton");
     plateButton.classList.toggle("buttonReady");
+    plateImage();
   }
 }
+function plateImage() {
+  var tack = document.createElement("div");
+  tack.className = "platefood";
+  tack.id = "Plated Food";
+  var TACK = document.getElementById("plateImage").appendChild(tack);
+
+  var tackCount = document.getElementById("plateFood").value;
+  var tackcount = tackCount + 1;
+  document.getElementById("plateFood").value = tackcount;
+
+  document.getElementById("platefood").innerHTML = tackCount;
+
+  var addReward = document.getElementById("platefood");
+
+  if (addReward.innerHTML == "1") {
+    document.getElementById("makeImage").innerHTML = "00";
+    document.getElementById("makeFood").value = "1";
+
+    var plateButton = document.getElementById("plateButton");
+    plateButton.classList.toggle("buttonReady");
+    plateImage();
+  }
+}
+
