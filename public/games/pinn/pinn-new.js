8b7684bf20c48ff802f9111767d16941280838a1
Glitch (mtcg) <none>
2021-11-16 19:44:43 +0000

    👧💧 Checkpoint ./public/games/pinn/pinn-new.js:5646401/14047 ./public/games/pinn/beta/index.html:5646401/144

--- public/games/pinn/pinn-new.js
+++ public/games/pinn/pinn-new.js
@@ -1,59 +1,13 @@
-const makeFood = document.getElementByID("makeFood");
-const plateFood = document.getElementByID("plateFood");
-const serveFood = document.getElementByID("serveFood");
+const madefood = document.getElementById("madefood");
+const madeFood = document.getElementById("madeFood");
+const platedfood = document.getElementById("platedfood");
+const platedFood = document.getElementById("madefood");
+const servedfood = document.getElementById("servedfood");
+const servedFood = document.getElementById("servedFood");
+const rewards = document.getElementById("rewards");
+const Rewards = document.getElementById("Rewards");
 
-
-function tackCount()
-
-
-function foodImage() {
-  var tack = document.createElement("div");
-  tack.className = "makefood";
-  tack.id = "Food";
-  var TACK = document.getElementById("foodimage").appendChild(tack);
-}
-
-function plateImage() {
-  var tack = document.createElement("div");
-  tack.className = "platefood";
-  tack.id = "PlatedFood";
-  var TACK = document.getElementById("platedimage").appendChild(tack);
-}
-
-function servedImage() {
-  var tack = document.createElement("div");
-  tack.className = "servedfood";
-  tack.id = "ServedFood";
-  var TACK = document.getElementById("servedimage").appendChild(tack);
-}
-
-function cashOut() {
-  var rewards = document.getElementById("rewards");
-
-  if (rewards.innerHTML == "0") {
-    alert("You Need A Full Token TO Cash Out!");
-  } else {
-    var txt;
-    var r = confirm("Quit Already?");
-    if (r == true) {
-      const data = {
-        dream:
-          dreamInput.value +
-          "-" +
-          dreamInputuser.value +
-          dreamInputplayer.value +
-          " Reward: "
-      };
-
-      appendNewDream(
-        dreamInput.value +
-          "-" +
-          dreamInputuser.value +
-          " Reward: " +
-          dreamInputplayer.value +
-          rewards.value
-      );
-      window.open("./");
-    } else txt = "You pressed Cancel!";
-  }
+function foodImage(){
+  
+  
 }
