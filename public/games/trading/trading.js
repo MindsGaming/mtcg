e3dd143b7e45da80139c437bbe6868be13abc233
Glitch (mtcg) <none>
2021-11-17 21:59:58 +0000

    🐓🎹 Checkpoint ./public/games/trading/trading.js:5646401/2995 ./public/games/trading/index.html:5646401/21666

--- public/games/trading/trading.js
+++ public/games/trading/trading.js
@@ -1,16 +1,15 @@
 var trading = "0.00001";
 
-
-function takeTour(){
-  var hide = document.getElementById("tour");
+function takeTour() {
+  var hide = document.getElementById("m1");
   hide.classList.toggle("hide");
-  
-  
-  
+  var show = document.getElementById("m2");
+  show.className = "messageBlock";
+  var tour = document.getElementById("tour");
+  tour.className = "touring";
 }
 
-
-function justPlay(){
+function justPlay() {
   var hide = document.getElementById("tour");
   hide.classList.toggle("hide");
 }
