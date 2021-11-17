5a7648fb756b5985ccf421040bbcc1af4b0ced0a
Glitch (mtcg) <none>
2021-11-17 17:01:48 +0000

    🍗🐷 Checkpoint ./public/games/pinn/index.html:5646401/11832 ./public/games/pinn/pinn.js:5646401/4144 ./public/games/pawn/index.html:5646401/5861

--- public/games/pinn/pinn.js
+++ public/games/pinn/pinn.js
@@ -1,57 +1,128 @@
-function createPinn() {
+function foodImage() {
   var tack = document.createElement("div");
-  tack.className = "tack";
-  tack.id = "PINN";
-  var TACK = document.getElementById("tack").appendChild(tack);
+  tack.className = "makefood";
+  tack.id = "Made Food";
+  var TACK = document.getElementById("foodImage").appendChild(tack);
 
-  var tackCount = document.getElementById("tackCount").value;
+  var tackCount = document.getElementById("makeFood").value;
   var tackcount = tackCount + 1;
-  document.getElementById("tackCount").value = tackcount;
+  document.getElementById("makeFood").value = tackcount;
 
-  document.getElementById("total").innerHTML = tackCount;
+  document.getElementById("makefood").innerHTML = tackCount + "%";
 
-  var addReward = document.getElementById("total");
+  var addReward = document.getElementById("makefood");
 
-  if (addReward.innerHTML == "100") {
-    var rewards = document.getElementById("rewards").value;
-    var rewardscount = rewards + 1;
-    document.getElementById("rewards").value = rewardscount;
-    var rewardstotal = document.getElementById("rewardstotal");
-    rewardstotal.innerHTML = rewardscount;
-    document.getElementById("total").innerHTML = "00";
-    document.getElementById("tackCount").value = "1";
+  if (addReward.innerHTML == "100%") {
+    document.getElementById("makefood").innerHTML = "100%";
+    document.getElementById("makeFood").value = "1";
+
+    var plateButton = document.getElementById("plateButton");
+    plateButton.className = "buttonReady";
+    var readyButton = document.getElementById("foodButton");
+    readyButton.classList.toggle("hide");
+  }
+  TACK.className = "makefood";
+}
+
+function platedImage() {
+  var check = document.getElementById("makefood");
+
+  if (check.innerHTML == "100%") {
+    check.innerHTML = "0%";
+    var tack = document.createElement("div");
+    tack.className = "platedfood";
+    tack.id = "Plated Food";
+    var TACK = document.getElementById("platedImage").appendChild(tack);
+
+    var tackCount = document.getElementById("platedFood").value;
+    var tackcount = tackCount + 1;
+    document.getElementById("platedFood").value = tackcount;
+    document.getElementById("platedfood").innerHTML = tackcount;
+
+    var foodButton = document.getElementById("foodButton");
+    foodButton.className = "buttonReady";
+    var serveButton = document.getElementById("serveButton");
+    serveButton.className = "buttonReady";
+  } else {
+    alert("Make Some More Food");
+  }
+}
+
+function servedImage() {
+  var check = document.getElementById("platedfood");
+
+  if ((check.innerHTML == "1", "2", "3", "4", "5")) {
+    var tackCount = document.getElementById("servedFood").value;
+    var tackcount = tackCount + 1;
+    document.getElementById("servedFood").value = tackcount;
+    document.getElementById("servedfood").innerHTML = tackCount;
+    document.getElementById("Rewards").value = tackcount;
+    document.getElementById("rewards").innerHTML = tackCount;
+
+    var tackCount2 = document.getElementById("platedFood").value;
+    var tackcount2 = tackCount2 - 1;
+    document.getElementById("platedFood").value = tackcount2;
+    document.getElementById("platedfood").innerHTML = tackcount2;
+
+    if (check.innerHTML == "0") {
+      var serveButton = document.getElementById("serveButton");
+      serveButton.className = "hide";
+      rewardCheck();
+    }
+  } else {
+    alert("Make Some More Food");
+  }
+}
+function rewardCheck() {
+  var check = document.getElementById("servedfood");
+  var cashOut = document.getElementById("cashOut");
+  cashOut.className = "buttonReady";
+
+  if ((check.innerHTML == "1", "2", "3", "4", "5")) {
+    var rewards = document.getElementById("rewards");
+    rewards.innerHTML = check.innerHTML;
+    document.getElementById("Rewards").value = check.value;
   }
 }
 
 function cashOut() {
   var rewards = document.getElementById("rewards");
-  var total = document.getElementById("total");
+  var Rewards = document.getElementById("Rewards");
+  var servedFood = document.getElementById("servedFood");
+  var platedFood = document.getElementById("platedFood");
+  var makeFood = document.getElementById("makeFood");
+  var servedfood = document.getElementById("servedfood");
+  var platedfood = document.getElementById("platedfood");
+  var makefood = document.getElementById("makefood");
 
-  if (rewards.innerHTML == "0") {
-    alert("You Need A Full Token TO Cash Out!");
-  } else {
-    var txt;
-    var r = confirm("Quit Already?");
-    if (r == true) {
-      const data = {
-        dream:
-          dreamInput.value +
-          "-" +
-          dreamInputuser.value +
-          dreamInputplayer.value +
-          " Reward: "
-      };
+  var txt;
+  var r = confirm("Start A New Shift?");
+  if (r == true) {
+    rewards.innerHTML = "0";
+    Rewards.value = "0";
+    servedFood.value = "1";
+    platedFood.value = "1";
+    makeFood.value = "30%";
+    servedfood.innerHTML = "1";
+    platedfood.innerHTML = "1";
+    makefood.innerHTML = "30%";
 
-      appendNewDream(
-        dreamInput.value +
-          "-" +
-          dreamInputuser.value +
-          " Reward: " +
-          dreamInputplayer.value +
-          rewards.value +
-          total.innerHTML
-      );
-      window.open("./");
-    } else txt = "You pressed Cancel!";
-  }
+    appendNewDream(
+      dreamInput.value +
+        "-" +
+        dreamInputuser.value +
+        " Reward: " +
+        dreamInputplayer.value +
+        Rewards.value +
+        ".00"
+    );
+  } else txt = "You pressed Cancel!";
+}
+
+function quit() {
+  var txt;
+  var r = confirm("Quit Your Job?");
+  if (r == true) {
+    window.open("/", "_self");
+  } else txt = "Get back to work";
 }
