f8dcdb26891ee1f81bc38de2fe284aa7ef6a75d6
Glitch (mtcg) <none>
2021-11-17 21:48:48 +0000

    💳👮 Checkpoint ./public/games/trading/index.html:5646401/26973 ./public/games/trading/trading.js:5646401/252

--- public/games/trading/trading.js
+++ public/games/trading/trading.js
@@ -1,1 +1,16 @@
 var trading = "0.00001";
+
+
+function takeTour(){
+  var hide = document.getElementById("tour");
+  hide.classList.toggle("hide");
+  
+  
+  
+}
+
+
+function justPlay(){
+  var hide = document.getElementById("tour");
+  hide.classList.toggle("hide");
+}
