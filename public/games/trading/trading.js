d6be55cb4251320f45008e0cd9d1fe911d8320fc
Glitch (mtcg) <none>
2021-11-20 00:50:15 +0000

    🐝👦 Checkpoint ./public/games/trading/trader.js:5646401/477 ./public/games/trading/trading.js:5646401/18837

--- public/games/trading/trading.js
+++ public/games/trading/trading.js
@@ -270,3 +270,33 @@ function usersIdea() {
   button.className = "hide";
   openMenu();
 }
+
+var price = [ "1.00", "2.00", "3.00", "4.00"];
+var amount = "100.00";
+
+function HOLDERme() {
+  var p1 = document.getElementByID("p1");
+  var a1 = document.getElementById("a1");
+  var s1 = document.getElementById("s1");
+
+  var PRICE = price[Math.floor(Math.random() * price.length)];
+  var AMOUNT = amount[Math.floor(Math.random() * amount.length)];
+
+  var SUM = PRICE * AMOUNT;
+
+  alert(PRICE);
+}
+
+function sellOne() {
+  var p1 = document.getElementById("p1");
+  var a1 = document.getElementById("a1");
+  var s1 = document.getElementById("s1");
+
+  var PRICE = price[Math.floor(Math.random() * price.length)];
+  var AMOUNT = amount[Math.floor(Math.random() * amount.length)];
+
+  var SUM = PRICE * AMOUNT;
+  p1.innerHTML = PRICE + " |";
+  a1.innerHTML = AMOUNT + " |";
+  s1.innerHTML = SUM + " |";
+}
