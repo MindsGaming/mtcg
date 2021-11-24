4dc938a884f48fb1a5f290e8b242212fb2b42b7d
Glitch (mtcg) <none>
2021-11-24 17:42:23 +0000

    🍝💳 Checkpoint ./views/index.html:5646401/80594 ./public/js/disable.js:5646401/61

--- public/js/disable.js
+++ public/js/disable.js
@@ -1,14 +1,14 @@
 // client-side js
 // run by the browser each time your view template referencing it is loaded
 function cashTokens() {
-  alert("Cashout is currently disabled. Try Again Later");
+  alert("Log IS Full. Try Again Later");
 }
 
 const disable = document.getElementById("submit-dream");
 disable.addEventListener("click", cleaning);
 
 function cleaning() {
-  alert("Claims Are Being Processed Try Again Later");
+  alert("Claims Need To Be Processed Try Again Later");
 }
 
 function guest() {
