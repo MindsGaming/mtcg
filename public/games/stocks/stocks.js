97abe09b1a780c000859753760191470c94536b4
Glitch (mtcg) <none>
2021-11-25 01:14:10 +0000

    🐋🔖 Checkpoint ./public/games/stocks/index.html:5646401/8591 ./public/games/stocks/stocks.js:5646401/2802

--- public/games/stocks/stocks.js
+++ public/games/stocks/stocks.js
@@ -1,1 +1,26 @@
-/// holder // 
+/// holder //
+
+const minutesLabel = document.getElementById("marketOne");
+const secondsLabel = document.getElementById("marketTwo");
+
+var totalSeconds = 0;
+setInterval(setTime, 1000);
+
+function setTime() {
+  ++totalSeconds;
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+  secondsLabel.value = pad(totalSeconds % 60);
+  minutesLabel.value = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+  var valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
+
+
