d66852642b5bd3c4b39f3444708c8d29a3b5390b
Glitch (mtcg) <none>
2021-11-25 02:12:17 +0000

    ⛑🐈 Checkpoint ./public/games/stocks/stocks.js:5646401/46 ./public/games/stocks/index.html:5646401/16

--- public/games/stocks/stocks.js
+++ public/games/stocks/stocks.js
@@ -28,12 +28,12 @@ if (secondsOne.value == "5") {
   var tackCount = document.getElementById("stockOnes").value;
   var tackcount = tackCount + 1;
   document.getElementById("stockOnes").value = tackcount;
-  document.getElementById("stockOnes").innerHTML = tackcount;
+  document.getElementById("stockones").innerHTML = tackcount;
 
   if (tackCount == "59") {
-    const stockone = document.getElementById("stockOne").innerHTML;
+    const stockone = document.getElementById("stockOne").value;
     var stockcount = stockone + 1;
     document.getElementById("stockOne").value = stockcount;
-    document.getElementById("stockOne").innerHTML = stockcount;
+    document.getElementById("stockone").innerHTML = stockcount;
   }
 }
