aa7812e758fbb040a4f91a70d8a31e2f2e16da99
Glitch (mtcg) <none>
2021-11-25 02:46:02 +0000

    💽💆 Checkpoint ./public/games/stocks/index.html:5646401/5161 ./public/games/stocks/stocks.js:5646401/1496

--- public/games/stocks/stocks.js
+++ public/games/stocks/stocks.js
@@ -1,36 +1,21 @@
-const minutesLabel = document.getElementById("minutes");
-const secondsLabel = document.getElementById("seconds");
-
-var totalSeconds = 0;
-setInterval(setTime, 3141);
-
-function setTime() {
-  ++totalSeconds;
-  secondsLabel.innerHTML = pad(totalSeconds % 60);
-  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
-
-  progress();
-}
-
-function pad(val) {
-  var valString = val + "";
-  if (valString.length < 2) {
-    return "0" + valString;
-  } else {
-    return valString;
-  }
-}
-
-/* Click Miner */
+/* Start Gamer */
+/* Click Timer */
 function progress() {
-  var ClickMiner = document.getElementById("stocks").value;
+  var secondsTimer = document.getElementById("secondsTimer").value;
   var move = +1;
   var mine = +0;
   var buffer = move + mine;
-  progress = ClickMiner + buffer;
-  document.getElementById("stocks").value = progress;
+  progress = secondsTimer + buffer;
+  document.getElementById("secondsTimer").value = progress;
 
   if (progress == "99") {
-    alert("testing");
+  var minutesTimer = document.getElementById("secondsTimer").value;
+     var move = +1;
+  var mine = +0;
+  var buffer = move + mine;
+  progress = minutesTimer + buffer;
+  document.getElementById("Timer").value = progress;
   }
 }
+
+
