a10c9a34d5e826158b0d8c60641aa5714e91a650
Glitch (mtcg) <none>
2021-11-25 02:00:17 +0000

    ⛵️🐦 Checkpoint ./public/games/stocks/stocks.js:5646401/9373 ./public/games/stocks/index.html:5646401/10387

--- public/games/stocks/stocks.js
+++ public/games/stocks/stocks.js
@@ -1,6 +1,9 @@
 const minutesLabel = document.getElementById("minutes");
 const secondsLabel = document.getElementById("seconds");
 
+const minutesOne = document.getElementById("minutesOne");
+const secondsOne = document.getElementById("secondsOne");
+
 var totalSeconds = 0;
 setInterval(setTime, 3141);
 
@@ -8,6 +11,8 @@ function setTime() {
   ++totalSeconds;
   secondsLabel.innerHTML = pad(totalSeconds % 60);
   minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+  secondsOne.value = pad(totalSeconds % 60);
+  minutesOne.vlaue = pad(parseInt(totalSeconds / 60));
 }
 
 function pad(val) {
@@ -19,17 +24,16 @@ function pad(val) {
   }
 }
 
-const stockones = document.getElementById("StockOnes");
+if (secondsOne.value == "5") {
+  var tackCount = document.getElementById("stockOnes").value;
+  var tackcount = tackCount + 1;
+  document.getElementById("stockOnes").value = tackcount;
+  document.getElementById("stockOnes").innerHTML = tackcount;
 
-function stockOne() {
-  if (secondsLabel.innerHTML == "20") {
-    var tackCount = document.getElementById("stockOnes").innerHTML;
-    var tackcount = tackCount + 1;
-    document.getElementById("stockOnes").value = tackcount;
-    document.getElementById("stockOnes").innerHTML = tackcount;
-    
-    if(tackCount == "59"){
-      
-    }
+  if (tackCount == "59") {
+    const stockone = document.getElementById("stockOne").innerHTML;
+    var stockcount = stockone + 1;
+    document.getElementById("stockOne").value = stockcount;
+    document.getElementById("stockOne").innerHTML = stockcount;
   }
 }
