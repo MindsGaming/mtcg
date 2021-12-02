1dc2bc409bbfce6dc2105416d934a7d4031b050d
Glitch (mtcg) <none>
2021-12-02 15:48:37 +0000

    ❄️😇 Checkpoint ./public/games/match/index.html:5646401/8756 ./public/games/match/beta.html:5646401/3573 ./public/games/match/match.js:5646401/12848

---
+++ public/games/match/match.js
@@ -0,0 +1,166 @@
+function startBombs() {
+  Bombs();
+  var ID = [
+    "aOne",
+    "aTwo",
+    "aThree",
+    "bOne",
+    "bTwo",
+    "bThree",
+    "cOne",
+    "cTwo",
+    "cThree"
+  ];
+  const random = ID[Math.floor(Math.random() * ID.length)];
+
+  const create = document.createElement("div");
+  create.className = "pixel";
+  create.title = "Dig For Rewards";
+  create.id = random;
+  create.name = random;
+  var Create = document.getElementById("bombs").appendChild(create);
+  Create.addEventListener("click", Digging);
+
+  function Digging() {
+    if (Create.name == "token") {
+      Create.className = "reward";
+      rewardSound();
+    }
+    if (Create.name == "rock") {
+      Create.className = "rock";
+      rockSound();
+    }
+    if (Create.name == "bomb") {
+      Create.className = "bomb";
+      bombSound();
+    }
+  }
+
+  function Bombs() {
+    var ID = [
+      "aOne",
+      "aTwo",
+      "aThree",
+      "bOne",
+      "bTwo",
+      "bThree",
+      "cOne",
+      "cTwo",
+      "cThree"
+    ];
+    const random = ID[Math.floor(Math.random() * ID.length)];
+
+    var create = document.createElement("div");
+    create.className = "pixel";
+    create.title = "Dig For Rewards";
+    create.name = random;
+    create.id = random;
+    create.name = random;
+    var Create = document.getElementById("bombs").appendChild(create);
+    Create.addEventListener("click", Digging);
+    startBombs();
+
+    if (create.id == "aOne") {
+      var match = 
+      
+      startBombs();
+    }
+    if (create.id == "rock") {
+      startBombs();
+    }
+    if (create.id == "token") {
+      startBombs();
+    }
+
+    function Digging() {
+      if (Create.name == "token") {
+        Create.className = "reward";
+        rewardSound();
+      }
+      if (Create.name == "rock") {
+        Create.className = "rock";
+        rockSound();
+      }
+      if (Create.name == "bomb") {
+        Create.className = "bomb";
+        bombSound();
+      }
+    }
+  }
+}
+
+function bombSound() {
+  var music = document.createElement("audio");
+  music.src =
+    "https://cdn.glitch.me/f23d0d76-dc88-4f4e-afe2-9bd56ac40b28%2FSmall-explosion-in-the-distance-sound-effect.mp3";
+  music.volume = 0.3;
+  music.autoPlay = false;
+  music.title = "playing";
+  var Music = document.getElementById("bomb").appendChild(music);
+  played();
+  var Create = document.getElementById("bomb");
+  Create.addEventListener("click", played);
+
+  function played() {
+    if (Music.title == "playing") {
+      music.play(), 5000;
+      Music.title = "playing";
+    } else {
+      music.pause(), 5000;
+      Music.title = "paused";
+    }
+  }
+}
+
+function rockSound() {
+  var music = document.createElement("audio");
+  music.src =
+    "https://cdn.glitch.me/f23d0d76-dc88-4f4e-afe2-9bd56ac40b28%2Fboing-sound-effect.mp3";
+  music.volume = 0.3;
+  music.autoPlay = false;
+  music.title = "playing";
+  var Music = document.getElementById("rock").appendChild(music);
+  played();
+  var Create = document.getElementById("rock");
+  Create.addEventListener("click", played);
+
+  function played() {
+    if (Music.title == "playing") {
+      music.play(), 5000;
+      Music.title = "playing";
+    } else {
+      music.pause(), 5000;
+      Music.title = "paused";
+    }
+  }
+}
+function rewardSound() {
+  var music = document.createElement("audio");
+  music.src =
+    "https://cdn.glitch.me/f23d0d76-dc88-4f4e-afe2-9bd56ac40b28%2Fcash-register-sound-effect.mp3";
+  music.volume = 0.3;
+  music.autoPlay = false;
+  music.title = "playing";
+  var Music = document.getElementById("token").appendChild(music);
+  played();
+  var Create = document.getElementById("token");
+  Create.addEventListener("click", played);
+
+  function played() {
+    if (Music.title == "playing") {
+      music.play(), 5000;
+      Music.title = "playing";
+    } else {
+      music.pause(), 5000;
+      Music.title = "paused";
+    }
+  }
+  score();
+}
+
+function score() {
+  var tackCount = document.getElementById("SCORE").value;
+  var tackcount = tackCount + 1;
+  document.getElementById("SCORE").value = tackcount;
+  document.getElementById("score").innerHTML = tackcount;
+}
