1c951faffdcd4ee7492d4c63cb76efb9482d7909
Glitch (mtcg) <none>
2021-12-02 16:35:38 +0000

    ⛺️🚡 Checkpoint ./public/games/match/match.js:5646401/8788 ./public/games/match/index.html:5646401/20

--- public/games/match/match.js
+++ public/games/match/match.js
@@ -1,4 +1,43 @@
 function startBombs() {
+  var matchOne = ["aOne", "aTwo", "aThree"];
+
+  var matchTwo = ["bOne", "bTwo", "bThree"];
+
+  var matchThree = ["cOne", "cTwo", "cThree"];
+
+  var ID = [matchOne, matchTwo, matchThree];
+  const random = ID[Math.floor(Math.random() * ID.length)];
+
+  const create = document.createElement("div");
+  create.className = "pixel";
+  create.title = "Match Images";
+  create.id = random;
+  create.name = random;
+  var Create = document.getElementById("bombs").appendChild(create);
+  Create.addEventListener("click", matching);
+  Create.addEventListener("click", createImage);
+
+  function createImage() {
+    if (Create.id == matchOne) {
+      Create.className = "rock";
+    }
+    if (Create.id == matchTwo) {
+      Create.className = "bomb";
+    }
+    if (Create.id == matchOne) {
+      Create.className = "reward";
+    }
+    if (Create.id == ",,") {
+      Create.className = "reward";
+    }
+  }
+
+  function matching() {}
+
+  Bombs();
+}
+
+function Bombs() {
   var matchOne = [
     document.getElementById("aOne"),
     document.getElementById("aTwo"),
@@ -16,22 +55,19 @@ function startBombs() {
     document.getElementById("cTwo"),
     document.getElementById("cThree")
   ];
-  
-  var ID = [
-    matchOne,
-    matchTwo,
-    matchThree
-  ];
+
+  var ID = [matchOne, matchTwo, matchThree];
   const random = ID[Math.floor(Math.random() * ID.length)];
-  
+
   const create = document.createElement("div");
   create.className = "pixel";
-  create.title = "Dig For Rewards";
+  create.title = "Match Images";
   create.id = random;
   create.name = random;
   var Create = document.getElementById("bombs").appendChild(create);
-  Create.addEventListener("click", match);
+  Create.addEventListener("click", matching);
 
-  
-  
+  function matching() {}
+
+  startBombs();
 }
