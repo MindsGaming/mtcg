19ecf11e31be1327485b323a011b493b6e94c478
Glitch (mtcg) <none>
2021-12-02 17:11:39 +0000

    🐃🌷 Checkpoint ./public/games/match/match.js:5646401/15425

--- public/games/match/match.js
+++ public/games/match/match.js
@@ -1,19 +1,6 @@
 function startBombs() {
-  var matchOne = ["aOne", "aTwo", "aThree"];
-  var matchTwo = ["bOne", "bTwo", "bThree"];
-  var matchThree = ["cOne", "cTwo", "cThree"];
-
-  var ID = [
-    "aOne",
-    "aTwo",
-    "aThree",
-    "bOne",
-    "bTwo",
-    "bThree",
-    "cOne",
-    "cTwo",
-    "cThree"
-  ];
+ 
+  var ID = ["aOne", "aTwo", "aThree"];
   const random = ID[Math.floor(Math.random() * ID.length)];
 
   const create = document.createElement("div");
@@ -26,23 +13,38 @@ function startBombs() {
   Create.addEventListener("click", createImage);
 
   function createImage() {
-    if (Create.id == "aone") {
+    if (Create.id == "aOne") {
       Create.className = "rock";
+      var tackCount = document.getElementById("matchOne").value;
+      var tackcount = tackCount + 1;
+      document.getElementById("matchOne").value = tackcount;
+      
     }
-     if (Create.id == "aone") {
-      Create.className = "rock";
-    } if (Create.id == "aone") {
-      Create.className = "rock";
-    }
-    if (Create.id == matchTwo) {
+
+    if (Create.id == "aTwo") {
       Create.className = "bomb";
+      var tackCount = document.getElementById("matchTwo").value;
+      var tackcount = tackCount + 1;
+      document.getElementById("matchOne").value = tackcount;
+  
     }
-    if (Create.id == matchThree) {
+    if (Create.id == "aThree") {
       Create.className = "reward";
+      var tackCount = document.getElementById("matchThree").value;
+      var tackcount = tackCount + 1;
+      document.getElementById("matchOne").value = tackcount;
+    
     }
   }
 
-  function matching() {}
+  function matching() {
+          var tackCount = document.getElementById("matchOne").value;
+if (tackCount == "3"){
+ document.getElementById("score").innerHTML = tackCount;  
+}
+    
+    
+  }
 
   Bombs();
 }
