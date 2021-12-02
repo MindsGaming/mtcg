73d8ec04c5da0e25cb9db3e6620d69bec5529fcc
Glitch (mtcg) <none>
2021-12-02 16:23:37 +0000

    🍰☺️ Checkpoint ./public/games/match/match.js:5646401/2385

--- public/games/match/match.js
+++ public/games/match/match.js
@@ -1,7 +1,37 @@
 function startBombs() {
+  var matchOne = [
+    document.getElementById("aOne"),
+    document.getElementById("aTwo"),
+    document.getElementById("aThree")
+  ];
+
+  var matchTwo = [
+    document.getElementById("bOne"),
+    document.getElementById("bTwo"),
+    document.getElementById("bThree")
+  ];
+
+  var matchThree = [
+    document.getElementById("cOne"),
+    document.getElementById("cTwo"),
+    document.getElementById("cThree")
+  ];
   
-  var matchOne = [  ];
+  var ID = [
+    matchOne,
+    matchTwo,
+    matchThree
+  ];
+  const random = ID[Math.floor(Math.random() * ID.length)];
+  
+  const create = document.createElement("div");
+  create.className = "pixel";
+  create.title = "Dig For Rewards";
+  create.id = random;
+  create.name = random;
+  var Create = document.getElementById("bombs").appendChild(create);
+  Create.addEventListener("click", match);
+
+  
   
 }
-
-
