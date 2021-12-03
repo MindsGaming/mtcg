c4d358fbb98766a13adca4079710856f3e596d93
Glitch (mtcg) <none>
2021-12-03 00:10:18 +0000

    🌪🏖 Checkpoint ./public/games/match/match.js:5646401/64850

--- public/games/match/match.js
+++ public/games/match/match.js
@@ -1,46 +1,99 @@
 function startBombs() {
-  var ID = ["bomb", "rock", "token"];
+  Bombs();
+  var ID = [
+    "bomb",
+    "rock",
+    "token",
+    "rock",
+    "bomb",
+    "bomb",
+    "rock",
+    "token",
+    "bomb",
+    "rock",
+    "rock",
+    "bomb",
+    "bomb",
+    "rock"
+  ];
   const random = ID[Math.floor(Math.random() * ID.length)];
 
   const create = document.createElement("div");
   create.className = "pixel";
   create.title = "Dig For Rewards";
   create.id = random;
+  create.name = random;
   var Create = document.getElementById("bombs").appendChild(create);
   Create.addEventListener("click", Digging);
-  Create.addEventListener("click", checkCount);
 
   function Digging() {
     if (Create.name == "token") {
       Create.className = "reward";
-      Create.title = "fliped";
       rewardSound();
-      var tackCount = document.getElementById("matchOne").value;
-      var score = document.getElementById("score").innerHTML;
-
-      var tackcount = tackCount + 1;
-      var tackcount2 = score + 1;
-
-      document.getElementById("matchOne").value = tackcount;
-      document.getElementById("score").innerHTML = tackcount2;
-      
     }
     if (Create.name == "rock") {
       Create.className = "rock";
-      Create.title = "fliped";
       rockSound();
     }
     if (Create.name == "bomb") {
       Create.className = "bomb";
-      Create.Title = "fliped";
       bombSound();
     }
   }
 
-  function checkCount() {
-          var matchOne = document.getElementById("matchOne").value;
+  function Bombs() {
+    var ID = [
+      "bomb",
+      "rock",
+      "token",
+      "rock",
+      "bomb",
+      "bomb",
+      "rock",
+      "token",
+      "bomb",
+      "rock",
+      "rock",
+      "bomb",
+      "bomb",
+      "rock"
+    ];
+    const random = ID[Math.floor(Math.random() * ID.length)];
 
-    
+    var create = document.createElement("div");
+    create.className = "pixel";
+    create.title = "Dig For Rewards";
+    create.name = random;
+    create.id = random;
+    create.name = random;
+    var Create = document.getElementById("bombs").appendChild(create);
+    Create.addEventListener("click", Digging);
+    startBombs();
+
+    if (create.id == "bomb") {
+      startBombs();
+    }
+    if (create.id == "rock") {
+      startBombs();
+    }
+    if (create.id == "token") {
+      startBombs();
+    }
+
+    function Digging() {
+      if (Create.name == "token") {
+        Create.className = "reward";
+        rewardSound();
+      }
+      if (Create.name == "rock") {
+        Create.className = "rock";
+        rockSound();
+      }
+      if (Create.name == "bomb") {
+        Create.className = "bomb";
+        bombSound();
+      }
+    }
   }
 }
 
