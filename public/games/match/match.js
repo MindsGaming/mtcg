4c10d8c392c2b4c6434f3f4beb7a7bcf32682a91
Glitch (mtcg) <none>
2021-12-08 22:35:09 +0000

    🍁👩‍❤️‍💋‍👩 Checkpoint ./public/games/match/index.html:5646401/28157 ./public/games/match/match.js:5646401/3507

---
+++ public/games/match/match.js
@@ -0,0 +1,15 @@
+var item1 = document.getElementById("item1");
+var item2 = document.getElementById("item2");
+var item3 = document.getElementById("item3");
+var item4 = document.getElementById("item4");
+var item5 = document.getElementById("item5");
+var item6 = document.getElementById("item6");
+var item7 = document.getElementById("item7");
+var item8 = document.getElementById("item8");
+var item9 = document.getElementById("item9");
+function items() {
+  
+  
+  
+  
+}
