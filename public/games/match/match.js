d8538b29fd101779a2bee464777b4ca190d8fa53
Glitch (mtcg) <none>
2021-12-08 23:57:19 +0000

    🍥📉 Checkpoint ./public/games/match/match.js:5646401/9115

--- public/games/match/match.js
+++ public/games/match/match.js
@@ -1,5 +1,25 @@
 function startBombs() {
   Bombs();
+  const minutesLabel = document.getElementById("minutes");
+  const secondsLabel = document.getElementById("seconds");
+
+  var totalSeconds = 0;
+  setInterval(setTime, 3141);
+
+  function setTime() {
+    ++totalSeconds;
+    secondsLabel.innerHTML = pad(totalSeconds % 60);
+    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+  }
+
+  function pad(val) {
+    var valString = val + "";
+    if (valString.length < 2) {
+      return "0" + valString;
+    } else {
+      return valString;
+    }
+  }
 }
 
 function Bombs() {
