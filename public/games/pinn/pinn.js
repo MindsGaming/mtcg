5d73d4d3f0d48f47901dad105112b30bf26a9d76
Glitch (mtcg) <none>
2021-12-30 21:06:52 +0000

    👍🎑 Checkpoint ./public/games/pinn/pinn.js:5646401/32670 ./public/games/pinn/index.html:5646401/2544

--- public/games/pinn/pinn.js
+++ public/games/pinn/pinn.js
@@ -104,6 +104,56 @@ function quit() {
 }
 
 function claimBurgers() {
- var burgers = document.getElementById("Rewards");
-  
+  var burgers = document.getElementById("Rewards");
+  var xtra = document.getElementById("XTRA");
+  var REWARDS = burgers.value + "." + xtra;
+  if (dreamInput.value == "") {
+    alert("Login To Earn Rewards");
+    otherSound();
+  } else {
+    if (minutesLabel.innerHTML == "00") {
+      alert("You Need A Full Token To Claim Rewards");
+      oopsSound();
+    } else {
+      const data = {
+        dream: dreamInput.value + " " + dreamToken.innerHTML + ":" + REWARDS
+      };
+
+      fetch("/addDream", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" }
+      })
+        .then(res => res.json())
+        .then(response => {
+          console.log(JSON.stringify(response));
+        });
+      // get dream value and add it to the list
+      dreams.push(
+        dreamInput.value + " " + dreamToken.innerHTML + ":" + REWARDS
+      );
+      appendNewDream(
+        dreamInput.value + " " + dreamToken.innerHTML + ":" + REWARDS
+      );
+      reset();
+      selectToken();
+
+      if (dreamToken.innerHTML == "GAMER") {
+        dreamToken.innerHTML = "DooBetter";
+        setInterval(setTime, 15000);
+      }
+
+      if (dreamToken.innerHTML == "DooBetter") {
+        dreamToken.innerHTML = "ECLIPSE";
+        setInterval(setTime, 13141);
+      }
+
+      if (dreamToken.innerHTML == "ECLIPSE") {
+        dreamToken.innerHTML = "GAMER";
+        setInterval(setTime, 18981);
+      }
+      rewardSound();
+    }
+  }
 }
+
