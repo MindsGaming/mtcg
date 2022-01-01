5e6c9fa384d58a23e429c8952bbe7de5206efc33
Glitch (mtcg) <none>
2022-01-01 02:38:54 +0000

    🏍🎂 Checkpoint ./public/guest/index.html:5646401/5311 ./public/fly.js:5646401/418

--- public/fly.js
+++ public/fly.js
@@ -202,3 +202,13 @@ function vrMode() {
     }
   }
 }
+
+/* Guest PAGE */
+
+ var createHUB = document.createElement("iframe");
+  createHUB.src =
+    "https://hubs.mozilla.com/EJFaEcZ?embed_token=d4f6c2c54a1a684be3668b366724fb05";
+  createHUB.className = "SuperSplash";
+  createHUB.allow = "microphone; camera; vr; speaker;";
+  var CREATEHUB = document.getElementById("guest").appendChild(createHUB);
+  var openHub = document.getElementById("guest");
