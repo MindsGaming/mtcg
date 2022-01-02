84cde68497b25a91613294a266c54bb36a801bd5
Glitch (mtcg) <none>
2022-01-02 02:20:13 +0000

    🎣🚖 Checkpoint ./public/eggs/eggs.js:5646401/14900 ./public/eggs/bountyhunter.html:5646401/46

--- public/eggs/eggs.js
+++ public/eggs/eggs.js
@@ -1,64 +1,65 @@
 // client-side js
 // run by the browser each time your view template referencing it is loaded
 
-console.log("#MindsGaming EGG FOUND");
+const dreams = [];
 
-function claimEGG() {
-  var base = document.getElementById("base");
-  var reward = base.title;
+// define variables that reference elements on our page
+const dreamsForm = document.forms[0];
+const dreamInput = dreamsForm.elements["dream"];
+const dreamsList = document.getElementById("dreams");
+const clearButton = document.querySelector("#clear-dreams");
+const base = document.querySelector("base");
+var reward = base.title;
 
-  // request the dreams from our app's sqlite database
-  fetch("/getDreams", {})
+// request the dreams from our app's sqlite database
+fetch("/getDreams", {})
+  .then(res => res.json())
+  .then(response => {
+    response.forEach(row => {
+      appendNewDream(row.dream);
+    });
+  });
+
+// a helper function that creates a list item for a given dream
+const appendNewDream = dream => {
+  const newListItem = document.createElement("li");
+  newListItem.innerText = dream;
+  dreamsList.appendChild(newListItem);
+};
+
+// listen for the form to be submitted and add a new dream when it is
+dreamsForm.onsubmit = event => {
+  // stop our form submission from refreshing the page
+  event.preventDefault();
+
+  const data = { dream: dreamInput.value + "EGG Reward: " + reward };
+
+  fetch("/addDream", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json" }
+  })
     .then(res => res.json())
     .then(response => {
-      response.forEach(row => {
-        appendNewDream(row.dream);
-      });
+      console.log(JSON.stringify(response));
     });
+  // get dream value and add it to the list
+  dreams.push(dreamInput.value + "EGG Reward: " + reward);
+  appendNewDream(dreamInput.value + "EGG Reward: " + reward);
 
-  // a helper function that creates a list item for a given dream
-  const appendNewDream = dream => {
-    const newListItem = document.createElement("li");
-    newListItem.innerText = dream;
-    dreamsList.appendChild(newListItem);
-  };
+  // reset form
+  dreamInput.value = "";
+  dreamInput.focus();
+  const claimMe = document.querySelector("claimMe");
+  claimMe.style = "display: none";
+  alert("Claimed!");
+};
 
-  // listen for the form to be submitted and add a new dream when it is
-  dreamsForm.onsubmit = event => {
-    // stop our form submission from refreshing the page
-    event.preventDefault();
-
-    const data = { dream: dreamInput.value + "EGG Reward: " + reward };
-
-    fetch("/addDream", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" }
-    })
-      .then(res => res.json())
-      .then(response => {
-        console.log(JSON.stringify(response));
-      });
-    // get dream value and add it to the list
-    dreams.push(dreamInput.value + "EGG Reward: " + reward);
-    appendNewDream(dreamInput.value + "EGG Reward: " + reward);
-
-    // reset form
-    dreamInput.value = "";
-    dreamInput.focus();
-    var egg = document.getElementById("claimMe");
-    egg.className = "hide";
-    rewardSound();
-  };
-}
-
-function MoMint() {
-  var placeholder = document.getElementById("EGG");
-  placeholder.placeholder = "Momint User";
-  var moMint = document.getElementById("moMint");
-  moMint.style = "display: none";
-  var btnTXT = document.getElementById("claimEGG");
-  btnTXT.innerHTML = "Claim Momint NFT";
-  var base = document.getElementById("claimEGG");
-  base.title = "Momint Claim";
-}
+clearButton.addEventListener("click", event => {
+  fetch("/clearDreams", {})
+    .then(res => res.json())
+    .then(response => {
+      console.log("cleared dreams");
+    });
+  dreamsList.innerHTML = "";
+});
