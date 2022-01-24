fba6535f9afafcf894019f316335087549cc6232
Glitch (mtcg) <none>
2022-01-24 17:08:09 +0000

    ✌️🍴 Checkpoint ./public/slots/index.html:5646401/4744 ./public/slots/slots.js:5646401/25688 ./public/slots/style.css:5646401/12272 ./public/games/client.js:5646401/9204 ./public/games/index.html:5646401/799

---
+++ public/games/client.js
@@ -0,0 +1,252 @@
+// Version
+const version = document.getElementById("version");
+version.innerHTML = "V0.15";
+version.required;
+version.title = version.innerHTML;
+version.id = version.innerHTML;
+
+/// Client & Timer
+
+console.log("Welcome To #MindsGaming Blockchain Rewards");
+const minutesLabel = document.getElementById("timeM");
+const secondsLabel = document.getElementById("timeS");
+var superToken = document.getElementById("myToken");
+var dreamToken = document.getElementById("myToken");
+var dreamWallet = document.getElementById("myWallet");
+var prize = document.getElementById("prize")
+var PRIZE = document.getElementById("PRIZE")
+selectToken();
+
+var totalSeconds = 0;
+setInterval(setTime, 10000);
+
+function setTime() {
+  ++totalSeconds;
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+
+  var levelups = document.getElementById("levelUp");
+
+  if (minutesLabel.innerHTML == "11") {
+    if (secondsLabel.innerHTML == "01") {
+      levelups.value = "10";
+      levelupPop();
+    }
+  }
+  if (minutesLabel.innerHTML == "21") {
+    if (secondsLabel.innerHTML == "01") {
+      levelups.value = "20";
+      levelupPop();
+    }
+  }
+  if (minutesLabel.innerHTML == "31") {
+    if (secondsLabel.innerHTML == "01") {
+      levelups.value = "30";
+      levelupPop();
+    }
+  }
+  if (minutesLabel.innerHTML == "41") {
+    if (secondsLabel.innerHTML == "01") {
+      levelups.value = "40";
+      levelupPop();
+    }
+  }
+  if (minutesLabel.innerHTML == "51") {
+    if (secondsLabel.innerHTML == "01") {
+      levelups.value = "50";
+      levelupPop();
+    }
+  }
+
+ if (minutesLabel.innerHTML == "61") {
+   alert("You Maxed Out Rewards, Are You Still Active?");
+   claimUpdate();
+    if (secondsLabel.innerHTML == "01") {
+    }
+  }
+}
+
+function pad(val) {
+  var valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
+
+const dreams = [];
+
+// define variables that reference elements on our page
+const dreamsForm = document.forms[0];
+const dreamInput = dreamsForm.elements["dream"];
+const dreamsList = document.getElementById("dreams");
+const clearButton = document.querySelector("#clear-dreams");
+const myToken = document.getElementById("REWARDTOKEN");
+
+// request the dreams from our app's sqlite database
+fetch("/getDreams", {})
+  .then((res) => res.json())
+  .then((response) => {
+    response.forEach((row) => {
+      appendNewDream(row.dream);
+    });
+  });
+
+// a helper function that creates a list item for a given dream
+const appendNewDream = (dream) => {
+  const newListItem = document.createElement("li");
+  newListItem.innerText = dream;
+  dreamsList.appendChild(newListItem);
+};
+
+// listen for the form to be submitted and add a new dream when it is
+dreamsForm.onsubmit = (event) => {
+  // stop our form submission from refreshing the page
+  event.preventDefault();
+
+  const data = {
+    dream: dreamInput.value + ":" + version.innerHTML,
+  };
+
+  fetch("/addDream", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => res.json())
+    .then((response) => {
+      console.log(JSON.stringify(response));
+    });
+  // get dream value and add it to the list
+  dreams.push(dreamInput.value + " :Loged In");
+  appendNewDream(dreamInput.value + " :Loged In");
+
+  // reset form
+  claimUpdate();
+  var closeLogin = document.getElementById("getLogin");
+  closeLogin.className = "hide";
+  walletChange();
+};
+
+clearButton.addEventListener("click", (event) => {
+  fetch("/clearDreams", {})
+    .then((res) => res.json())
+    .then((response) => {
+      console.log("cleared dreams");
+    });
+  dreamsList.innerHTML = "";
+});
+
+// Claim
+
+function claimUpdate() {
+  if (dreamInput.value == "") {
+    alert("Login To Earn Rewards");
+    otherSound();
+  } else {
+    if (minutesLabel.innerHTML == "00") {
+      alert("You Need A Full Token To Claim Rewards");
+      oopsSound();
+    } else {
+      var REWARDS = PRIZE.value + "." + secondsLabel.innerHTML;
+      const data = {
+        dream:
+          dreamInput.value +
+          " " +
+          version.innerHTML +
+          dreamToken.innerHTML +
+          ":" +
+          REWARDS,
+      };
+
+      fetch("/addDream", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((res) => res.json())
+        .then((response) => {
+          console.log(JSON.stringify(response));
+        });
+      // get dream value and add it to the list
+      dreams.push(
+        dreamInput.value + " " + dreamToken.innerHTML + ":" + REWARDS
+      );
+      appendNewDream(
+        dreamInput.value + " " + dreamToken.innerHTML + ":" + REWARDS
+      );
+      levelUp();
+      reset();
+      rewardSound();
+    }
+  }
+}
+
+function reset() {
+  var zero = "00";
+  document.getElementById("timeM").innerHTML = zero;
+  document.getElementById("timeS").innerHTML = zero;
+  document.getElementById("prize").innerHTML = zero;
+  document.getElementById("PRIZE").value = zero;
+  totalSeconds = "0";
+}
+
+// LevelUp
+
+function levelUp() {
+  var tackCount = document.getElementById("levelUp").value;
+  var tackcount = tackCount + 1;
+  document.getElementById("levelUp").value = tackcount;
+
+  if (tackcount == "11") {
+    dreamToken.innerHTML = "ECLIPSE";
+    dreamToken.title = "ECLIPSE";
+    dreamToken.className = "eclipseIMG";
+    tackCount.value = "12";
+  }
+
+  if (tackcount == "21") {
+    dreamToken.innerHTML = "DooBetter";
+    dreamToken.title = "DooBetter";
+    dreamToken.className = "doobetterIMG";
+    tackCount.value = "22";
+  }
+
+  if (tackcount == "31") {
+    dreamToken.innerHTML = "GTPC";
+    dreamToken.title = "GTPC";
+    dreamToken.className = "gtpcIMG";
+    tackCount.value = "32";
+  }
+
+  if (tackcount == "41") {
+    dreamToken.innerHTML = "GAMER";
+    dreamToken.title = "GAMER";
+    dreamToken.className = "gamerIMG";
+    tackCount.value = "42";
+  }
+
+  if (tackcount == "51") {
+    dreamToken.innerHTML = "DragonToken";
+    dreamToken.title = "DragonToken";
+    dreamToken.className = "dragontokenIMG";
+    tackCount.value = "0";
+  }
+}
+
+function walletChange() {
+  var x = document.getElementById("myWallet");
+  x.innerHTML = dreamInput.value;
+}
+
+function selectToken() {
+  var tokens = ["DragonToken"];
+  const newtoken = tokens[Math.floor(Math.random() * tokens.length)];
+  var a = document.getElementById("myToken");
+  a.innerHTML = newtoken;
+  dreamToken.innerHTML = a.innerHTML;
+  dreamToken.title = "DragonToken";
+  dreamToken.className = "dragontokenIMG";
+  dreamToken.innerHTML = newtoken;
+}
---
+++ public/games/slots/slots.js
@@ -0,0 +1,196 @@
+var timeM = document.getElementById("timeM");
+var timeS = document.getElementById("timeS");
+
+var totalSeconds = 0;
+
+function setTime() {
+  ++totalSeconds;
+  timeS.innerHTML = pad(totalSeconds % 60);
+  timeM.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+  var valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
+
+var boost = setInterval(setTime, 1000);
+var spinerOne = document.getElementById("spinOne");
+var spinerTwo = document.getElementById("spinTwo");
+var spinerThree = document.getElementById("spinThree");
+
+var baseImage = document.createElement("img");
+var baseimageTwo = document.createElement("img");
+var baseimageThree = document.createElement("img");
+baseImage.src =
+  "https://cdn.minds.com/icon/568132636652875781/medium/1630521552";
+baseImage.title = "MTCG";
+baseimageTwo.src =
+  "https://cdn.minds.com/icon/568132636652875781/medium/1630521552";
+baseimageTwo.title = "MTCG";
+baseimageThree.src =
+  "https://cdn.minds.com/icon/568132636652875781/medium/1630521552";
+baseimageThree.title = "MTCG";
+baseImage.className = "spined";
+baseimageTwo.className = "spined";
+baseimageThree.className = "spined";
+
+var BASEIMAGE = document.getElementById("spinOne").appendChild(baseImage);
+var BASEIMAGETWO = document.getElementById("spinTwo").appendChild(baseimageTwo);
+var BASEIMAGETHREE = document
+  .getElementById("spinThree")
+  .appendChild(baseimageThree);
+
+var imageOne = document.createElement("img");
+imageOne.src =
+  "https://cdn.glitch.me/76239ef0-e5c6-4e29-9830-26d4ec2dfc0b%2FGAMER.jpeg";
+imageOne.className = "spined";
+
+var imageTwo = document.createElement("img");
+imageTwo.src =
+  "https://cdn.minds.com/icon/463565168681431051/medium/1617704314";
+imageTwo.className = "spined";
+
+var imageThree = document.createElement("img");
+imageThree.src =
+  "https://cdn.minds.com/icon/944965270747750406/medium/1627757450";
+imageThree.className = "spined";
+
+var imageFour = document.createElement("img");
+imageFour.src =
+  "https://cdn.minds.com/icon/822733901683236867/medium/1621185080";
+imageFour.className = "spined";
+
+var imageFive = document.createElement("img");
+imageFive.src =
+  "https://cdn.glitch.global/f23d0d76-dc88-4f4e-afe2-9bd56ac40b28/ZomboMeme%2017012022164751.jpg";
+imageFive.className = "spined";
+
+var spinme = document.getElementById("spinMe");
+var credits = document.getElementById("credits");
+var CREDITS = document.getElementById("CREDITS");
+var prize = document.getElementById("prize");
+var PRIZE = document.getElementById("PRIZE");
+
+function spin() {
+  if (credits.innerHTML == "0") {
+    alert("Load Rewards");
+  } else {
+    var remove = 5;
+    var total = credits.innerHTML - remove;
+    var prizetotal = credits.innerHTML - remove;
+    credits.innerHTML = total;
+    prize.innerHTML = prizetotal;
+    prize.value = prizetotal;
+    anamation();
+
+    if (BASEIMAGE.title == BASEIMAGETWO.title) {
+      if (BASEIMAGETWO.title == BASEIMAGETHREE.title) {
+        var booster = 10;
+        var total = credits.innerHTML + booster;
+        credits.innerHTML = total;
+      }
+    } else {
+      var remove = 5;
+      var total = credits.innerHTML - remove;
+      credits.innerHTML = total;
+      if (credits.innerHTML == "11") {
+        CREDITS.value = "0";
+      }
+    }
+  }
+}
+
+function anamation() {
+  one();
+  function one() {
+    BASEIMAGE.src = imageOne.src;
+    var next = setTimeout(two, 1000);
+    two();
+  }
+  function two() {
+    BASEIMAGE.src = imageTwo.src;
+    var next = setTimeout(three, 2000);
+    three();
+  }
+  function three() {
+    BASEIMAGE.src = imageThree.src;
+    var next = setTimeout(four, 3000);
+    four();
+  }
+  function four() {
+    BASEIMAGE.src = imageFour.src;
+    var next = setTimeout(five, 4000);
+    five();
+  }
+  function five() {
+    BASEIMAGE.src = imageFive.src;
+    random();
+  }
+}
+
+function random() {
+  var images = [imageOne, imageTwo, imageThree, imageFour, imageFive];
+  const newImage = images[Math.floor(Math.random() * images.length)];
+  const newimageTwo = images[Math.floor(Math.random() * images.length)];
+  const newimageThree = images[Math.floor(Math.random() * images.length)];
+
+  BASEIMAGE.src = newImage.src;
+  BASEIMAGETWO.src = newimageTwo.src;
+  BASEIMAGETHREE.src = newimageTwo.src;
+
+  BASEIMAGE.title = newImage.src;
+  BASEIMAGETWO.title = newimageTwo.src;
+  BASEIMAGETHREE.title = newimageTwo.src;
+  checkRewards();
+}
+
+function checkRewards() {
+  if (BASEIMAGE.title == BASEIMAGETWO.title) {
+    if (BASEIMAGETWO.title == BASEIMAGETHREE.title) {
+      makeRewards();
+    }
+  }
+}
+
+function makeRewards() {
+  var boost = 1;
+  var total = boost + CREDITS.value;
+  var prizetotal = boost + PRIZE.value;
+  CREDITS.value = total;
+  credits.innerHTML = total;
+  prize.innerHTML = prizetotal;
+
+  if (credits.innerHTML == "6") {
+    var boost = 6;
+    var total = CREDITS.value - boost;
+    CREDITS.value = total;
+    credits.innerHTML = total;
+  }
+}
+
+function loadRewards() {
+  if (timeM.innerHTML == "00") {
+    alert("You Need A Full Reward");
+  } else {
+    var current = timeM.innerHTML;
+    var cred = CREDITS.value;
+    var total = current + cred;
+    CREDITS.value = total;
+    credits.innerHTML = CREDITS.value;
+    reset();
+  }
+}
+
+function claimPrize() {
+  if (dreamInput.value == "") {
+    var login = document.getElementById("getLogin");
+    login.className = "SuperSplash";
+  } else {
+    claimUpdate();
+  }
+}
