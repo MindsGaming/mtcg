9947cd794108ee51c2f6dece0ab01ac4ec04ac22
Glitch (mtcg) <none>
2022-02-02 19:54:04 +0000

    👴🎫 Checkpoint ./public/games/creator/creator.js:5646401/4765

--- public/games/creator/creator.js
+++ public/games/creator/creator.js
@@ -1,12 +1,15 @@
-        var div = document.createElement("img");
-        div.src = "https://cdn.glitch.global/f23d0d76-dc88-4f4e-afe2-9bd56ac40b28/MTCGblank.jpg";
-        div.style = "width: 40%; height: 70%; display:block;";
-        document.getElementById("newcard").appendChild(div);
-    
-function background(){
+function background() {
   let inputVal = document.getElementById("url").value;
-        var div = document.createElement("img");
-        div.src = inputVal;
-        div.className = "Mesh";
-        document.getElementById("newcard").appendChild(div);
+  var div = document.createElement("img");
+  div.src = inputVal;
+  div.style = "width: 40%; height: 70%; display:block; margin:3px; opacity:.8;";
+  div.id = "MTCG";
+  document.getElementById("newcard").appendChild(div);
+
+  var div2 = document.createElement("img");
+  div2.src =
+    "https://cdn.glitch.global/f23d0d76-dc88-4f4e-afe2-9bd56ac40b28/MTCGblank.jpg";
+  div2.style = "width: 40%; height: 70%; display:block;";
+  div2.id = "background";
+  document.getElementById("MTCG").appendChild(div2);
 }
