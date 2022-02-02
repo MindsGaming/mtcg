b75f0fec0cbfd34e7ee824376c38af04ba828d88
Glitch (mtcg) <none>
2022-02-02 19:19:56 +0000

    ⚾️🐁 Checkpoint ./public/games/creator/creator.js:5646401/1752

--- public/games/creator/creator.js
+++ public/games/creator/creator.js
@@ -1,11 +1,14 @@
-var card = document.getElementById("newcard");
-var newcard = document.createElement("img");
-newcard.src = card.value;
-newcard.id = "newcard";
-var background = document.getElementById("newcard");
-var backgroundImage = document.createElement("img");
-backgroundImage.src = "/images/coins.png";
+let newcard = document.getElementById("newcard");
+var card = document.createElement("img");
+card.src = "https://cdn.glitch.com/c7248de5-f10d-4345-bfbd-d33f86010870%2FZomboMeme%2030032021045454.jpg";
+card.id= "MTCG"
+document.getElementById("newcard").appendChild(card);
 
-if (newcard.value == ""){
-  newcard.src = "/"
+
+function background(){
+  let newcard = document.getElementById("newcard").value;
+var card = document.createElement("img");
+card.src = newcard;
+card.id= "MTCG"
+document.getElementById("newcard").appendChild(card);
 }
