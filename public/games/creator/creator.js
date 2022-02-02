aee36cfc2283817a056741958b0bd1406b5a90a3
Glitch (mtcg) <none>
2022-02-02 19:42:56 +0000

    🎥😜 Checkpoint ./public/games/creator/creator.js:5646401/2470

--- public/games/creator/creator.js
+++ public/games/creator/creator.js
@@ -1,19 +1,12 @@
-let newcard = document.getElementById("newcard");
-var card = document.createElement("img");
-card.src =
-  "https://cdn.glitch.global/f23d0d76-dc88-4f4e-afe2-9bd56ac40b28/MTCGblank.jpg";
-card.style = "width: 30%; height: 70%; display: none;";
-card.id = "MTCG";
-document.getElementById("newcard").appendChild(card);
-
-function background() {
-card.style = "width: 30%; height: 70%; display: block;";
-
-  let backgroundImage = document.getElementById("newcard").value;
-  var cardBackground = document.createElement("img");
-  cardBackground.src = newcard;
-  cardBackground.style = "background-color: white; width: 30%; height: 70%; dispaly: block; opacity: 0.8;";
-  cardBackground.id = "Printed";
-
-  document.getElementById("MTCG").appendChild(cardBackground);
+        var div = document.createElement("img");
+        div.src = "https://cdn.glitch.global/f23d0d76-dc88-4f4e-afe2-9bd56ac40b28/MTCGblank.jpg";
+        div.style = "width: 40%; height: 70%; display:block;";
+        document.getElementById("newcard").appendChild(div);
+    
+function background(){
+  let inputVal = document.getElementById("url").value;
+        var div = document.createElement("img");
+        div.src = inputVal;
+        div.className = "Mesh";
+        document.getElementById("newcard").appendChild(div);
 }
