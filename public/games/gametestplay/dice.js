bf6aaa599ca024fd115b81ea6a408e98d7d3849e
Glitch (mtcg) <none>
2022-02-02 18:45:40 +0000

    💠🍔 Checkpoint ./views/index.html:5646401/15008 ./public/games/index.html:5646401/9285

--- public/games/gametestplay/dice.js
+++
@@ -1,8 +0,0 @@
-play();
-function play() {
-  var game = document.getElementById("gametest");
-  var open = window.open("http://gametestplay.com/ball.html", "_self");
-  game.innerHTML = open;
-  game.style =
-    "position:fixed; display: block; top:0; left: 0; width: 50%; height: 50%; margin: -1px;";
-}
