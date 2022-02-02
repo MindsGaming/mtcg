c3004fb110ff26b990020bb9a479ec7e9b5d51d1
Glitch (mtcg) <none>
2022-02-02 21:12:53 +0000

    👠🕌 Checkpoint ./views/index.html:5646401/4 ./public/games/creator/creator.js:5646401/1111 ./public/games/creator/index.html:5646401/112

--- public/games/creator/creator.js
+++ public/games/creator/creator.js
@@ -5,18 +5,10 @@ function background() {
   if(inputVal == ""){
   }else {
      var div = document.createElement("img");
-  div.src = inputVal;
-  div.style = "width: 40%; height: 70%; display:block; margin:3px; opacity:.8;";
+  div.src = "/images/coins.png";
+  div.style = "width: 40%; height: 70%; display:block; margin:3px; opacity:.8; background-image: url('/images/coins.png')";
   div.id = "MTCG";
-  document.getElementById("newcard").appendChild(div);
-
-  var div2 = document.createElement("img");
-  div2.src =
-    "https://cdn.glitch.global/f23d0d76-dc88-4f4e-afe2-9bd56ac40b28/MTCGblank.jpg";
-  div2.style = "width: 40%; height: 70%; display:block;";
-  div2.id = "background";
-  document.getElementById("MTCG").appendChild(div2);
-  
+  document.getElementById("MTCG").appendChild(div);
     
   }
 }
