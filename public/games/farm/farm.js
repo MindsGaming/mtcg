6cbd227805b168442f7f4948547f7b8b1f60d8a6
Glitch (mtcg) <none>
2022-02-03 19:49:49 +0000

    🛍🎬 Checkpoint ./public/games/farm/farm.js:5646401/1476 ./public/games/farm/index.html:5646401/175

--- public/games/farm/farm.js
+++ public/games/farm/farm.js
@@ -1,11 +1,23 @@
-var farm = document.ElementCreate("div");
-farm.className = "block";
-farm.id = currentBlocks;
+var farm = document.createElement("div");
+farm.className = "pixel";
+farm.ID = "1";
 var FARM = document.getElementById("farm").appendChild(farm);
 
-var blocks = document.getElementById("blocks");
-
-var id = 1;
-var currentBlocks = blocks.value;
-var total = currentBlocks + id;
-document.getElementById("blocks").value = total;
+function blocks(){
+  
+  if (farm.ID == "100"){
+    
+  }
+  else {
+    var boost = 1;
+    var BLOCKS =document.getElementById("blocks");
+    var currentBlocks = BLOCKS.value;
+    var total = 
+    var farm = document.createElement("div");
+farm.className = "pixel";
+farm.ID = "1";
+var FARM = document.getElementById("farm").appendChild(farm);
+    
+  }
+  
+}
