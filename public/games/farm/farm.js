5bbd284c0809daff82d115c8ac8b88979c4d8d3a
Glitch (mtcg) <none>
2022-02-05 05:18:28 +0000

    🏣🛤 Checkpoint ./public/games/farm/farm.js:5646401/943 ./public/games/farm/index.html:5646401/21 ./public/games/farm/style.css:5646401/2782

--- public/games/farm/farm.js
+++ public/games/farm/farm.js
@@ -1,20 +1,13 @@
+
 var farm = document.getElementById("farm");
 
-block();
 
-function block() {
-  if (farm.title == "100") {
-  } else {
     var createFarm = document.createElement("div");
-    createFarm.className = "pixel";
-
+  createFarm.className = "pixel";
   var tackCount = document.getElementById("FARM").value;
   var tackcount = tackCount + 1;
   document.getElementById("FARM").value = tackcount;
 
     createFarm.id = tackcount;
-    farm.title = tackcount;
-    var FARM = document.getElementById("farm").appendChild(createFarm);
-    block();
-  }
-}
+
+
