8671256b33f694b7d101015b05c3c900924c16b1
Glitch (mtcg) <none>
2022-02-05 06:27:35 +0000

    🏃🏈 Checkpoint ./public/games/farm/farm.js:5646401/180

--- public/games/farm/farm.js
+++ public/games/farm/farm.js
@@ -1,20 +1,11 @@
 var farm = document.getElementById("FARM");
 var createFarm = document.createElement("div");
 
-var createSalt = document.createElement("salt");
-createFarm.addEventListener("click", salt);
-
-function salt(){
-createSalt.className = "salt";
-createSalt.id = "Salty"
-}
 
 addFarm();
 function addFarm() {
   if (farm.value == "90") {
   } else {
-    
-    
     var moreFarm = document.getElementById("FARM").value;
     var moreFarms = moreFarm + 1;
     document.getElementById("FARM").value = moreFarms;
@@ -24,9 +15,7 @@ function addFarm() {
     createFarm.title = moreFarms;
     var FARM = document.getElementById("farm").appendChild(createFarm);
     createFarm.addEventListener("click", item);
-
     addFarm();
-
   }
 }
 
