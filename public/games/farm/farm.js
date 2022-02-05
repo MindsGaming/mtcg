dffd56c570011949e594ed73f7564c5af3322114
Glitch (mtcg) <none>
2022-02-05 06:16:30 +0000

    🍦😻 Checkpoint ./public/games/farm/index.html:5646401/42 ./public/games/farm/style.css:5646401/45 ./public/games/farm/farm.js:5646401/711

--- public/games/farm/farm.js
+++ public/games/farm/farm.js
@@ -1,4 +1,14 @@
 var farm = document.getElementById("FARM");
+var createFarm = document.createElement("div");
+
+var createSalt = document.createElement("salt");
+createFarm.addEventListener("click", salt);
+
+function salt(){
+createSalt.className = "salt";
+createSalt.id = "Salty"
+}
+
 addFarm();
 function addFarm() {
   if (farm.value == "90") {
@@ -9,12 +19,18 @@ function addFarm() {
     var moreFarms = moreFarm + 1;
     document.getElementById("FARM").value = moreFarms;
     
-    var createFarm = document.createElement("div");
     createFarm.className = "pixel";
     createFarm.id = moreFarms;
     createFarm.title = moreFarms;
     var FARM = document.getElementById("farm").appendChild(createFarm);
+    createFarm.addEventListener("click", item);
+
     addFarm();
 
   }
+}
+
+function item(){
+  var salt = document.getElementById("salt");
+  createFarm.className  = "salt"
 }
