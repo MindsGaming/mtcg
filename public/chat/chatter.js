dac46c2631e0f4e7375ce253bb023e353f7130c3
Glitch (mtcg) <none>
2022-04-01 21:56:11 +0000

    💗🎹 Checkpoint ./public/chat/index.html:5646401/11094 ./public/chat/chatter.js:5646401/4893 ./public/chat/style.css:5646401/16344

---
+++ public/chat/chatter.js
@@ -0,0 +1,4 @@
+var createChat = document.createElement("iframe");
+createChat.src = "https://mtcg-chat.glitch.me";
+createChat.className = "fullscreen";
+document.getElementById("CHAT").appendChild(createChat);
