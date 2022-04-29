a6374e2e51ccb2f36b93c9bd6bda982aecffbf82
Glitch (mtcg) <none>
2022-04-29 19:11:29 +0000

    💍🌧 Checkpoint ./views/index.html:5646401/14909

--- public/chat/chatter.js
+++
@@ -1,4 +0,0 @@
-var createChat = document.createElement("iframe");
-createChat.src = "https://mtcg-chat.glitch.me";
-createChat.className = "fullscreen";
-document.getElementById("CHAT").appendChild(createChat);
