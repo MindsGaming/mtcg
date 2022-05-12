27f928d69cad430ca3e85388146cb353d0ef36b9
Glitch (mtcg) <none>
2022-05-12 18:32:21 +0000

    🏅🚿 Checkpoint ./public/games/hooks.js:5646401/3669 ./views/index.html:5646401/38 ./public/style.css:5646401/48

--- public/games/hooks.js
+++ public/games/hooks.js
@@ -1,6 +1,11 @@
 function emojiworld() {
   var getNOTICE = document.getElementById("NOTICE");
   var thisGame = document.getElementById("thisGame");
+  getNOTICE.className = "notice";
+  var rewardicon = document.getElementById("rewardicon");
+  var newrewardicon = document.getElementById("newrewardicon");
+  
+  var moveicon = 
+
   thisGame.innerHTML = " 'Emoji World'";
-  getNotice.className = ""
 }
