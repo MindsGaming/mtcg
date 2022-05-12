52d394e0470c115edfc7046d069e5589768d8631
Glitch (mtcg) <none>
2022-05-12 19:53:35 +0000

    🏟💫 Checkpoint ./public/games/hooks.js:5646401/531 ./views/index.html:5646401/48045 ./.glitch-assets:5646401/417

--- public/games/hooks.js
+++ public/games/hooks.js
@@ -21,3 +21,15 @@ function loadgametestplay() {
   thisGame.innerHTML = " 'GTPC'";
   window.open("http://gametestplay.com/");
 }
+
+function loadslots() {
+  var getNOTICE = document.getElementById("NOTICE");
+  var thisGame = document.getElementById("thisGame");
+  getNOTICE.className = "notice";
+  var rewardicon = document.getElementById("rewardicon");
+  var newrewardicon = document.getElementById("newrewardicon");
+  rewardicon.className = "rewardiconL";
+  newrewardicon = rewardicon;
+  thisGame.innerHTML = " 'Simple Slots'";
+  window.open("https://simple-slots.glitch.me/");
+}
