982c7cd34dfd57a511b1591bb359ccbb47310d5c
Glitch (mtcg) <none>
2022-05-12 19:45:03 +0000

    Rewound to commit 1f655aa8c349bc431a5cf0c4a0dbd37f378b0e16 + 0 edits

--- public/games/hooks.js
+++ public/games/hooks.js
@@ -10,18 +10,14 @@ function emojiworld() {
   window.open("https://emjoi-world.glitch.me/");
 }
 
-   <div
-            class="game"
-            title="GameTestPlay"
-            onclick="loadgametestplay();"
-            id="loadgametestplay"
-          >
-            <div class="gametext">
-              <b>GTPC</b>
-              <br />
-              <img
-                class="gameicon"
-                src="https://cdn.glitch.global/f23d0d76-dc88-4f4e-afe2-9bd56ac40b28/GTPC_20220120141137.png?v=1642705942337"
-              />
-            </div>
-          </div>
+function loadgametestplay() {
+  var getNOTICE = document.getElementById("NOTICE");
+  var thisGame = document.getElementById("thisGame");
+  getNOTICE.className = "notice";
+  var rewardicon = document.getElementById("rewardicon");
+  var newrewardicon = document.getElementById("newrewardicon");
+  rewardicon.className = "rewardiconL";
+  newrewardicon = rewardicon;
+  thisGame.innerHTML = " 'GTPC'";
+  window.open("http://gametestplay.com/");
+}
