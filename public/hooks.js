b98f6dd63eadb4e2f7e3c2c06f16d6fdd4b1255f
Glitch (mtcg) <none>
2022-05-12 16:35:13 +0000

    🏫🍰 Checkpoint ./public/hooks.js:5646401/6345

--- public/hooks.js
+++
@@ -1,8 +0,0 @@
-const submitwallet = document.getElementById("submit-dream");
-submitwallet.addEventListener("click", createHooks);
-
-function createHooks() {
-  var emojiwallet = document.getElementById("wallet");
-  var emojiform = document.getElementById("emojiform");
-  emojiform.style = "display: none;";
-}
