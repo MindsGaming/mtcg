458ebf0d0cc7a5f98cc435d87ed827c0296b606c
Glitch (mtcg) <none>
2022-05-12 18:20:30 +0000

    🎻🏡 Checkpoint ./views/index.html:5646401/27325 ./public/games/hooks.js:5646401/1017 ./public/style.css:5646401/91

--- public/games/hooks.js
+++ public/games/hooks.js
@@ -1,13 +1,6 @@
 function emojiworld() {
-  var notice = document.createElement("h1");
-  notice.align = "center";
-  notice.style =
-    "diplay: block; width: 60%; height: 60%; background-color: grey; color: red;";
-  var rewardicon = document.getElementById("rewardicon");
-  rewardicon.className = "rewardiconL";
-  var myNotice = "Playing 'Emoji World'";
-
-  var getemojiworld = document.getElementById("emojiworld");
-  getemojiworld.innerHTML = myNotice;
-  window.open("https://emjoi-world.glitch.me/")
+  var getNOTICE = document.getElementById("NOTICE");
+  var thisGame = document.getElementById("thisGame");
+  thisGame.innerHTML = " 'Emoji World'";
+  getNotice.className = ""
 }
