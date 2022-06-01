003ad0c60c9ac6581b3f667136bc8589f0a20b8a
Glitch (mtcg) <none>
2022-06-01 00:07:42 +0000

    🌘🛩 Checkpoint ./views/index.html:5646401/164451 ./public/cards.css:5646401/15 ./public/client.js:5646401/2

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 // Version
 const version = document.createElement("p");
-version.id = "V0.26";
+version.id = "V0.27";
 version.innerHTML = version.id;
 version.title = version.id;
 const VERSION = document.getElementById("version");
