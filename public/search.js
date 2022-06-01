d23b0448b31a89e3f25234b996c9e070bda2feec
Glitch (mtcg) <none>
2022-06-01 22:59:28 +0000

    ✂️🍩 Checkpoint ./public/search.js:5646401/6190 ./views/index.html:5646401/12

--- public/search.js
+++ public/search.js
@@ -6,5 +6,18 @@ const FartGasstats = document.getElementById("FartGasstats");
 const newlayout = [DINOMONSTERstats, FartGasstats];
 
 function byAttack() {
- document.getElementById("BIRDS").appendChild(POPOLVUH);
+  var clearTITLE = document.getElementById("SEARCH");
+  var clearAlbum = document.getElementById("BIRDS");
+  var refreshAlbum = clearAlbum.innerHTML;
+  
+  if (clearTITLE.title == "Birds/Air") {
+    clearAlbum.innerHTML = "";
+    clearTITLE.innerHTML = "ATTACK Value";
+    clearTITLE.title = "SEARCH";
+    
+    document.getElementById("BIRDS").appendChild(POPOLVUH);
+  } 
+  else {
+    clearAlbum.innerHTML = refreshAlbum;
+  }
 }
