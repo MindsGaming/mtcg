50197c2b0682794d50bcb67c5f7fd1d16f9f49fc
Glitch (mtcg) <none>
2022-06-01 22:24:36 +0000

    🎙📰 Checkpoint ./public/search.js:5646401/3415 ./views/index.html:5646401/30815

--- public/search.js
+++ public/search.js
@@ -1,5 +1,20 @@
-function test() {
-  var DINOMONSTERstats = document.getElementById("DIONMONSTERstats");
-  DINOMONSTERstats.value = DINOMONSTERstats.optimum;
-  alert(DINOMONSTERstats.value);
+var zero = 0;
+
+const DINOMONSTERstats = document.getElementById("DINOMONSTERstats");
+const DINOMONSTERATK = DINOMONSTERstats.high;
+const DINOMONSTERDEF = DINOMONSTERstats.low;
+const DINOMONSTERMANERGY = DINOMONSTERstats.min;
+const DINOMONSTERPRICE = DINOMONSTERstats.value;
+
+const FartGasstats = document.getElementById("FartGasstats");
+const FatGasATK = DINOMONSTERstats.high;
+const FartGasDEF = DINOMONSTERstats.low;
+const FartGasMANERGY = DINOMONSTERstats.min;
+const FartGasPRICE = DINOMONSTERstats.value;
+
+
+function byAttack(){
+var 
+  
+  
 }
