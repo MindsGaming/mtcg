b71cb682015e37e46ab4145f35615671232d212c
Glitch (mtcg) <none>
2022-06-03 20:52:40 +0000

    👝🐒 Checkpoint ./public/mtcg/mtcg.js:5646401/34522 ./public/mtcg/index.html:5646401/7385

--- public/mtcg/mtcg.js
+++ public/mtcg/mtcg.js
@@ -1,4 +1,92 @@
-/// CARDS
+const DINOMONSTER = document.createElement("img");
+DINOMONSTER.src =
+  "https://momintdev.blob.core.windows.net/uploads/a0f061e5-eebc-4d5e-a17f-c398d21f01ce.jpeg";
+DINOMONSTER.title = "DinoMonster";
+DINOMONSTER.className = "card";
+
+const DinoEggs = document.createElement("img");
+DinoEggs.src =
+  "https://momintdev.blob.core.windows.net/uploads/425c38c9-6b34-4c35-bb17-b3bba649cfd4.jpeg";
+DinoEggs.title = "DinoEggs";
+DinoEggs.className = "card";
+
+const FartGas = document.createElement("img");
+FartGas.src =
+  "https://momintdev.blob.core.windows.net/uploads/f0653389-45f9-4e05-a1da-88b38e4b8fb0.jpeg";
+FartGas.title = "FartGas";
+FartGas.className = "card";
+
+const AlienBeer = document.createElement("img");
+AlienBeer.src =
+  "https://momintdev.blob.core.windows.net/uploads/13dced15-b376-4488-be53-fba2fb642914.jpeg";
+AlienBeer.title = "AlienBeer";
+AlienBeer.className = "card";
+
+const APHRODITE = document.createElement("img");
+APHRODITE.src =
+  "https://momintdev.blob.core.windows.net/uploads/51819bca-ba1c-4ed2-a74d-2ac0dd1db9d2.jpeg";
+APHRODITE.title = "APHRODITE";
+APHRODITE.className = "card";
+
+const MoonRabbit = document.createElement("img");
+MoonRabbit.src =
+  "https://momintdev.blob.core.windows.net/uploads/d109dbea-0b06-46ff-b4fc-2b0446e1ec6e.jpeg";
+MoonRabbit.title = "MoonRabbit";
+MoonRabbit.className = "card";
+
+const BirdsNest = document.createElement("img");
+BirdsNest.src =
+  "https://momintdev.blob.core.windows.net/uploads/87911f7b-6ab0-42f9-9efe-11411277244e.jpeg";
+BirdsNest.title = "BirdsNest";
+BirdsNest.className = "card";
+
+const BullishFlyer = document.createElement("img");
+BullishFlyer.src =
+  "https://momintdev.blob.core.windows.net/uploads/629ce241-4ba9-4e03-a109-7e65f84a9b90.jpeg";
+BullishFlyer.title = "BullishFlyer";
+BullishFlyer.className = "card";
+
+const FlyingWisdom = document.createElement("img");
+FlyingWisdom.src =
+  "https://momintdev.blob.core.windows.net/uploads/4f7e62d3-d38c-4a00-a273-f8fcf46e676d.jpeg";
+FlyingWisdom.title = "FlyingWisdom";
+FlyingWisdom.className = "card";
+
+const POPOLVUH = document.createElement("img");
+POPOLVUH.src =
+  "https://momintdev.blob.core.windows.net/uploads/7af2b13d-5da5-49d3-97cf-5625f6f62a5b.jpeg";
+POPOLVUH.title = "POPOLVUH";
+POPOLVUH.className = "card";
+
+const TheMayans = document.createElement("img");
+TheMayans.src =
+  "https://momintdev.blob.core.windows.net/uploads/ba5db749-5638-47bb-93f3-953b850c472e.jpeg";
+TheMayans.title = "TheMayans";
+TheMayans.className = "card";
+
+const Mayflower = document.createElement("img");
+Mayflower.src =
+  "https://momintdev.blob.core.windows.net/uploads/ff1508f7-6c38-40a1-84ea-25ec9c97d1ba.jpeg";
+Mayflower.title = "TheMayans";
+Mayflower.className = "card";
+
+const Manergy = document.createElement("img");
+Manergy.src =
+  "https://momintdev.blob.core.windows.net/uploads/88dfb6d7-8cd0-43fb-9283-bb2fb6ca753d.jpeg";
+Manergy.title = "Manergy";
+Manergy.className = "card";
+
+const MAYDAY = document.createElement("img");
+MAYDAY.src =
+  "https://momintdev.blob.core.windows.net/uploads/756eccad-c783-47f5-8466-e842119a200e.jpeg";
+MAYDAY.title = "Mayflower";
+MAYDAY.className = "card";
+
+const SheetMusic = document.createElement("img");
+SheetMusic.src =
+  "https://momintdev.blob.core.windows.net/uploads/38e13978-41a6-443c-a7ac-4110d2e8776b.jpeg";
+SheetMusic.title = "SheetMusic";
+SheetMusic.className = "card";
 
 const dealCARDS = [
   "FartGas",
@@ -15,8 +103,29 @@ const dealCARDS = [
   "Manergy",
   "Mayflower",
   "MAYDAY",
-  "SheetMusic"
+  "SheetMusic",
 ];
+
+function DEALCARDS() {
+  var inHand = document.getElementById("inHand");
+
+  if (inHand.value == "5") {
+    alert("OOPSD");
+  } else {
+    var CARD1 = dealCARDS[Math.floor(Math.random() * dealCARDS.length)];
+    var CARD2 = dealCARDS[Math.floor(Math.random() * dealCARDS.length)];
+    var CARD3 = dealCARDS[Math.floor(Math.random() * dealCARDS.length)];
+
+    document.getElementById("playerCads").appendChild(CARD1);
+    document.getElementById("playerCads").appendChild(CARD2);
+    document.getElementById("playerCads").appendChild(CARD3);
+
+    var boost = 1;
+    var addcard = inHand.value;
+    var math = boost + addcard;
+    inHand.value = math;
+  }
+}
 
 /// METERS
 
