666304668fdab3608ecd82a386f856695892815a
Glitch (mtcg) <none>
2022-06-03 20:41:23 +0000

    ✊🏬 Checkpoint ./public/mtcg/mtcg.js:5646401/1474

--- public/mtcg/mtcg.js
+++ public/mtcg/mtcg.js
@@ -1,3 +1,25 @@
+/// CARDS
+
+const dealCARDS = [
+  "FartGas",
+  "DinoEggs",
+  "DINOMONSTER",
+  "AlienBeer",
+  "APHRODITE",
+  "MoonRabbit",
+  "BirdsNest",
+  "BullishFlyer",
+  "FlyingWisdom",
+  "POPOLVUH",
+  "TheMayans",
+  "Manergy",
+  "Mayflower",
+  "MAYDAY",
+  "SheetMusic"
+];
+
+/// METERS
+
 function playerMeter() {
   var playerTotal = document.getElementById("playerTotal");
   var playerMeter = document.getElementById("playerMeter");
