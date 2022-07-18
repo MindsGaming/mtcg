e877a58fffd737a2fd67c2dfdb114ff724df508f
Glitch (mtcg) <none>
2022-07-18 02:02:23 +0000

    💕🚖 Checkpoint ./public/help/index.html:5646401/43771 ./public/client.js:5646401/2

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 // Version
 const version = document.createElement("p");
-version.id = "V0.33";
+version.id = "V0.34";
 version.innerHTML = version.id;
 version.title = version.id;
 const VERSION = document.getElementById("version");
