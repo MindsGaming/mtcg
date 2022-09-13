8714acc079be364e78bb490562fe4d8bb32e8c7b
Glitch (mtcg) <none>
2022-09-13 19:26:16 +0000

    ✂️💂 Checkpoint ./public/balloon/pop.js:5646401/4599 ./public/balloon/index.html:5646401/6112

--- public/balloon/pop.js
+++ public/balloon/pop.js
@@ -1,4 +1,19 @@
-function check(){
-  var width = document.getElementById("balloon");
-  var
+const earn = ["1", "2", "3", "4", "5"];
+
+function check() {
+  var sniffwidth = document.getElementById("balloon");
+  var push = document.getElementById("BALLOON");
+  var width = sniffwidth.width;
+  push.value = width;
+
+  var boost = earn[Math.floor(Math.random() * earn.length)];
+  var pop = earn[Math.floor(Math.random() * earn.length)];
+
+  var math = boost + push.value;
+  sniffwidth.width = math;
+  sniffwidth.height = math;
+  var pushsniff = document.getElementById("BALLOON");
+  pushsniff.value = math;
+
+  alert(math);
 }
