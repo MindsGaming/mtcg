8784d75bb9e77605ca40988922277f8ab751af09
Glitch (mtcg) <none>
2022-09-14 20:37:25 +0000

    🍕🍯 Checkpoint ./public/old/client.js:5646401/12103 ./public/client.js:5646401/31566

--- public/client.js
+++ public/client.js
@@ -1,26 +1,7 @@
-// Version
-const version = document.createElement("p");
-version.id = "V0.35";
-version.innerHTML = version.id;
-version.title = version.id;
-const VERSION = document.getElementById("version");
-VERSION.innerHTML = version.id;
-VERSION.title = version.id;
-version.required;
-
-/// Client & Timer
-
-console.log("Welcome To #MindsGaming Blockchain Rewards");
-const minutesLabel = document.getElementById("minutes");
 const secondsLabel = document.getElementById("seconds");
-var superToken = document.getElementById("myToken");
-var dreamToken = document.getElementById("myToken");
-var dreamWallet = document.getElementById("myWallet");
-
-selectToken();
-
+const minutesLabel = document.getElementById("minutes");
 var totalSeconds = 0;
-setInterval(setTime, 9865);
+setInterval(setTime, 6000);
 
 function setTime() {
   ++totalSeconds;
@@ -28,51 +9,6 @@ function setTime() {
   minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
 
   var levelups = document.getElementById("levelUp");
-
-  if (minutesLabel.innerHTML == "11") {
-    if (secondsLabel.innerHTML == "01") {
-      levelups.value = "10";
-      levelupPop();
-    }
-  }
-  if (minutesLabel.innerHTML == "21") {
-    if (secondsLabel.innerHTML == "01") {
-      levelups.value = "20";
-      levelupPop();
-    }
-  }
-  if (minutesLabel.innerHTML == "31") {
-    if (secondsLabel.innerHTML == "01") {
-      levelups.value = "30";
-      levelupPop();
-    }
-  }
-  if (minutesLabel.innerHTML == "41") {
-    if (secondsLabel.innerHTML == "01") {
-      levelups.value = "40";
-      levelupPop();
-    }
-  }
-  if (minutesLabel.innerHTML == "51") {
-    if (secondsLabel.innerHTML == "01") {
-      levelups.value = "50";
-      levelupPop();
-    }
-  }
-
-  if (minutesLabel.innerHTML == "61") {
-    if (secondsLabel.innerHTML == "01") {
-      levelups.value = "60";
-      levelupPop();
-    }
-  }
-
-  if (minutesLabel.innerHTML == "71") {
-    alert("You Maxed Out Rewards, Are You Still Active?");
-    claimUpdate();
-    if (secondsLabel.innerHTML == "01") {
-    }
-  }
 }
 
 function pad(val) {
@@ -91,7 +27,7 @@ const dreamsForm = document.forms[0];
 const dreamInput = dreamsForm.elements["dream"];
 const dreamsList = document.getElementById("dreams");
 const clearButton = document.querySelector("#clear-dreams");
-const myToken = document.getElementById("REWARDTOKEN");
+var myWallet = document.getElementById("myWallet")
 
 // request the dreams from our app's sqlite database
 fetch("/getDreams", {})
@@ -115,7 +51,7 @@ dreamsForm.onsubmit = (event) => {
   event.preventDefault();
 
   const data = {
-    dream: dreamInput.value + ":" + version.innerHTML,
+    dream: dreamInput.value,
   };
 
   fetch("/addDream", {
@@ -132,8 +68,9 @@ dreamsForm.onsubmit = (event) => {
   appendNewDream(dreamInput.value + " :Loged In");
 
   // reset form
-
-  walletChange();
+  myWallet.id = dreamInput.value;
+  myWallet.innerHTML = dreamInput.value;
+  alert("DONE");
 };
 
 clearButton.addEventListener("click", (event) => {
@@ -144,133 +81,3 @@ clearButton.addEventListener("click", (event) => {
     });
   dreamsList.innerHTML = "";
 });
-
-// Claim
-
-function claimUpdate() {
-  if (dreamInput.value == "") {
-    alert("Login To Earn Rewards");
-    otherSound();
-  } else {
-    if (minutesLabel.innerHTML == "00") {
-      alert("You Need A Full Token To Claim Rewards");
-      oopsSound();
-    } else {
-      var REWARDS = minutesLabel.innerHTML + "." + secondsLabel.innerHTML;
-
-      const data = {
-        dream:
-          dreamInput.value +
-          " " +
-          version.innerHTML +
-          dreamToken.innerHTML +
-          ":" +
-          REWARDS,
-      };
-      fetch("/addDream", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: { "Content-Type": "application/json" },
-      })
-        .then((res) => res.json())
-        .then((response) => {
-          console.log(JSON.stringify(response));
-        });
-      // get dream value and add it to the list
-      dreams.push(
-        dreamInput.value + " " + dreamToken.innerHTML + ":" + REWARDS
-      );
-      appendNewDream(
-        dreamInput.value + " " + dreamToken.innerHTML + ":" + REWARDS
-      );
-
-      levelUp();
-      reset();
-      rewardSound();
-    }
-  }
-}
-
-function reset() {
-  var zero = "00";
-  document.getElementById("minutes").innerHTML = zero;
-  document.getElementById("seconds").innerHTML = zero;
-  totalSeconds = "0";
-}
-
-// LevelUp
-
-function levelUp() {
-  var tackCount = document.getElementById("levelUp").value;
-  var tackcount = tackCount + 1;
-  document.getElementById("levelUp").value = tackcount;
-
-  if (tackcount == "11") {
-    dreamToken.innerHTML = "DragonToken";
-    dreamToken.title = "DragonToken";
-    dreamToken.className = "dragontokenIMG";
-    tackCount.value = "12";
-  }
-
-  if (tackcount == "21") {
-    dreamToken.innerHTML = "ECLIPSE";
-    dreamToken.title = "ECLIPSE";
-    dreamToken.className = "eclipseIMG";
-    tackCount.value = "22";
-  }
-
-  if (tackcount == "31") {
-    dreamToken.innerHTML = "DooBetter";
-    dreamToken.title = "DooBetter";
-    dreamToken.className = "doobetterIMG";
-    tackCount.value = "32";
-  }
-
-  if (tackcount == "41") {
-    dreamToken.innerHTML = "GTPC";
-    dreamToken.title = "GTPC";
-    dreamToken.className = "gtpcIMG";
-    tackCount.value = "42";
-  }
-
-  if (tackcount == "51") {
-    dreamToken.innerHTML = "GAMER";
-    dreamToken.title = "GAMER";
-    dreamToken.className = "gamerIMG";
-    tackCount.value = "52";
-  }
-
-  if (tackcount == "61") {
-    dreamToken.innerHTML = "DarkMark";
-    dreamToken.title = "DarkMark";
-    dreamToken.className = "darkmarkIMG";
-    tackCount.value = "0";
-  }
-}
-
-function walletChange() {
-  var x = document.getElementById("myWallet");
-  x.innerHTML = dreamInput.value;
-}
-
-function selectToken() {
-  var tokens = ["Darkmark"];
-  const newtoken = tokens[Math.floor(Math.random() * tokens.length)];
-  var a = document.getElementById("myToken");
-  a.innerHTML = newtoken;
-  dreamToken.innerHTML = a.innerHTML;
-  dreamToken.title = "DarkMark";
-  dreamToken.className = "darkmarkIMG";
-  dreamToken.innerHTML = newtoken;
-}
-
-const checkrely = document.getElementById("myWallet");
-relyME();
-
-function relyME() {
-  if (checkrely.title == "userWallet") {
-  } else {
-    var mywallet = document.getElementById("myWallet");
-    mywallet.innerHTML = checkrely.innerHTML;
-  }
-}
