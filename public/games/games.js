9fffda46a781eb9579dff4cb0213657b5cd7bb5e
Glitch (mtcg-update) <none>
2022-09-15 17:31:36 +0000

    🎤🏂 Checkpoint ./public/games/beta.html:5646401/5517 ./public/games/games.js:5646401/9452 ./public/games/rpsbot/index.html:5646401/26 ./public/games/wack-me/index.html:5646401/36

--- public/games/games.js
+++ public/games/games.js
@@ -34,3 +34,38 @@ function theStock() {
       .appendChild(createHUB);
   }
 }
+
+function RPS() {
+  var game = document.getElementById("rps-build");
+  var title = document.getElementById("rps-title");
+  var info = document.getElementById("rps-info");
+  var button = document.getElementById("rps-btn");
+
+  if (game.title == "closed") {
+    title.className = "white";
+    info.className = "white";
+    game.title = "OPEN";
+    button.className = "float-right";
+    game.className = "splash";
+  } else {
+    if (game.title == "OPEN") {
+      title.className = "white";
+      info.className = "white";
+      game.title = "closed";
+      button.className = "hide";
+      game.className = "hide";
+    }
+  }
+
+  if (game.title == "build") {
+    title.className = "hide";
+    info.className = "hide";
+    game.title = "OPEN";
+    button.className = "float-right";
+    game.className = "splash";
+    var createHUB = document.createElement("iframe");
+    createHUB.src = "/games/rpsbot";
+    createHUB.className = "wrap";
+    var CREATEHUB = document.getElementById("rps-build").appendChild(createHUB);
+  }
+}
