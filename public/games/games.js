11234a90142ac8dbeaff57d9d426d37bc778324f
Glitch (mtcg-update) <none>
2022-09-15 16:56:27 +0000

    😺💪 Checkpoint ./public/games/games.js:5646401/6921 ./public/games/beta.html:5646401/7659

--- public/games/games.js
+++ public/games/games.js
@@ -1,27 +1,38 @@
 function theStock() {
-  var game = document.getElementById("thestock");
+  var game = document.getElementById("build-stock");
   var title = document.getElementById("stock-title");
   var info = document.getElementById("stock-info");
   var button = document.getElementById("stock-btn");
 
-  if (game.title == "preview") {
+  if (game.title == "build") {
     title.className = "hide";
     info.className = "hide";
     game.title = "OPEN";
     button.className = "float-right";
-    game.className = "Splash";
+    game.className = "splash";
     var createHUB = document.createElement("iframe");
     createHUB.src = "/games/the-stock";
     createHUB.className = "wrap";
-    createHUB.allow = "microphone; camera; vr; speaker;";
-    var CREATEHUB = document.getElementById("thestock").appendChild(createHUB);
-  } else {
+    var CREATEHUB = document
+      .getElementById("build-stock")
+      .appendChild(createHUB);
+  }
+  
+  else {
     if (game.title == "OPEN") {
       title.className = "white";
       info.className = "white";
-      game.title = "preview";
+      game.title = "closed";
       button.className = "hide";
-      game
+      game.className = "hide";
     }
+  }
+
+  if (game.title == "closed") {
+    title.className = "white";
+    info.className = "white";
+    game.title = "OPEN";
+    button.className = "float-right";
+    game.className = "splash";
   }
 }
