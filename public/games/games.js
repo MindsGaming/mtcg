c6d3f7e7adbc53d5cb909664125f5622ddb72e8a
Glitch (mtcg-update) <none>
2022-09-15 16:44:25 +0000

    👰🚟 Checkpoint ./public/games/beta.html:5646401/7315 ./public/games/games.js:5646401/4735 ./public/style.css:5646401/387

--- public/games/games.js
+++ public/games/games.js
@@ -2,10 +2,26 @@ function theStock() {
   var game = document.getElementById("thestock");
   var title = document.getElementById("stock-title");
   var info = document.getElementById("stock-info");
+  var button = document.getElementById("stock-btn");
 
   if (game.title == "preview") {
     title.className = "hide";
     info.className = "hide";
     game.title = "OPEN";
+    button.className = "float-right";
+    game.className = "Splash";
+    var createHUB = document.createElement("iframe");
+    createHUB.src = "/games/the-stock";
+    createHUB.className = "wrap";
+    createHUB.allow = "microphone; camera; vr; speaker;";
+    var CREATEHUB = document.getElementById("thestock").appendChild(createHUB);
+  } else {
+    if (game.title == "OPEN") {
+      title.className = "white";
+      info.className = "white";
+      game.title = "preview";
+      button.className = "hide";
+      game
+    }
   }
 }
