6b30687c62911f82cd06d56c98e79b76eb958d9b
Glitch (mtcg-update) <none>
2022-09-20 13:51:44 +0000

    🐯🍂 Checkpoint ./public/games/blocks/style.css:5646401/3879 ./public/games/blocks/index.html:5646401/80 ./public/games/blocks/blocks.js:5646401/4898

--- public/games/blocks/blocks.js
+++ public/games/blocks/blocks.js
@@ -1,67 +1,105 @@
-function buildBlock() {
-  const dreamsLists = document.getElementById("hold-blocks");
-  const blocks = document.getElementById("BLOCKS");
-  const build = document.getElementById("build-blocks");
-  build.className = "block";
-  const boost = 1;
-  const math = blocks.value + boost;
-  var spoofblocks = document.getElementById("BLOCKS");
-  spoofblocks.value = math;
+function buildBlocks()
 
-  fetch("/getDreams", {})
+
+// request the dreams from our app's sqlite database
+fetch("/getDreams", {})
+  .then((res) => res.json())
+  .then((response) => {
+    response.forEach((row) => {
+      appendNewDream(row.dream);
+    });
+  });
+
+var levelups = document.getElementById("levelUp");
+levelups.title = dreamInput.value;
+
+// a helper function that creates a list item for a given dream
+const appendNewDream = (dream) => {
+  const newListItem = document.createElement("li");
+  newListItem.innerText = dream;
+  newListItem.id = dreamInput.value;
+  dreamsList.appendChild(newListItem);
+};
+
+// listen for the form to be submitted and add a new dream when it is
+dreamsForm.onsubmit = (event) => {
+  // stop our form submission from refreshing the page
+  event.preventDefault();
+
+  const data = {
+    dream: dreamInput.value,
+  };
+
+  fetch("/addDream", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json" },
+  })
     .then((res) => res.json())
     .then((response) => {
-      response.forEach((row) => {
-        appendNewDream(row.dream);
-      });
+      console.log(JSON.stringify(response));
     });
+  // get dream value and add it to the list
+  dreams.push(dreamInput.value);
+  appendNewDream(dreamInput.value);
 
-  // a helper function that creates a list item for a given dream
-  const appendNewDream = (dream) => {
-    const newListItems = document.createElement("li");
-    newListItems.innerText = dream;
-    newListItems.id = math;
-    newListItems.className = "hide";
-    newListItems.onclick = "test();";
-    dreamsLists.appendChild(newListItems);
-  };
+  // reset form
+  var loginform = document.getElementById("login-form");
+  var walletlinks = document.getElementById("wallet-links");
+  myWallet.innerHTML = dreamInput.value;
+  loginform.className = "hide";
+  walletlinks.className = "display";
+  selectToken();
+};
 
-  // listen for the form to be submitted and add a new dream when it is
-  dreamsForm.onsubmit = (event) => {
-    // stop our form submission from refreshing the page
-    event.preventDefault();
+clearButton.addEventListener("click", (event) => {
+  fetch("/clearDreams", {})
+    .then((res) => res.json())
+    .then((response) => {
+      console.log("cleared dreams");
+    });
+});
 
-    const data = {
-      dream: dreamInput.value + "OWNS " + math,
-    };
+// Claim
 
-    fetch("/addDream", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" },
-    })
-      .then((res) => res.json())
-      .then((response) => {
-        console.log(JSON.stringify(response));
-      });
-    // get dream value and add it to the list
-    dreams.push(dreamInput.value);
-    appendNewDream(dreamInput.value);
+function claimUpdate() {
+  if (dreamInput.value == "") {
+    alert("Login To Earn Rewards");
+  } else {
+    if (minutesLabel.innerHTML == "00") {
+      alert("You Need A Full Token To Claim Rewards");
+    } else {
+      var REWARDS = minutesLabel.innerHTML + "." + secondsLabel.innerHTML;
 
-    // reset form
-  };
+      const data = {
+        dream: dreamInput.value + " " + dreamToken.innerHTML + ":" + REWARDS,
+      };
+      fetch("/addDream", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((res) => res.json())
+        .then((response) => {
+          console.log(JSON.stringify(response));
+        });
+      // get dream value and add it to the list
+      dreams.push(
+        dreamInput.value + "  " + dreamToken.innerHTML + ":" + REWARDS
+      );
+      appendNewDream(
+        dreamInput.value + "  " + dreamToken.innerHTML + ":" + REWARDS
+      );
 
-  clearButton.addEventListener("click", (event) => {
-    fetch("/clearDreams", {})
-      .then((res) => res.json())
-      .then((response) => {
-        console.log("cleared dreams");
-      });
-  });
+      reset();
+      levelUp();
+    }
+  }
 }
 
-function buyBlock() {
-  if (dreamInput.value == "") {
-    alert("LOGIN");
-  }
+function reset() {
+  var zero = "00";
+  document.getElementById("minutes").innerHTML = zero;
+  document.getElementById("seconds").innerHTML = zero;
+  totalSeconds = "0";
 }
