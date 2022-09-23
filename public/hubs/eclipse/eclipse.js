5c175c3647cd013ca6a4abfa5fd228898781bb7a
Glitch (mtcg) <none>
2022-09-23 14:51:30 +0000

    🚋🏃 Checkpoint

--- public/hubs/eclipse/eclipse.js
+++
@@ -1,6 +0,0 @@
-var createHUB = document.createElement("iframe");
-createHUB.src =
-  "https://hubs.mozilla.com/EJFaEcZ?embed_token=d4f6c2c54a1a684be3668b366724fb05";
-createHUB.className = "wrap";
-createHUB.allow = "microphone; camera; vr; speaker;";
-var CREATEHUB = document.getElementById("ECLIPSEHUB").appendChild(createHUB);
