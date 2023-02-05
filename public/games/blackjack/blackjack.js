82648e08cbe999ee7de1212deabf50abf7c9e994
Glitch (mtcg) <none>
2023-02-05 18:33:18 +0000

    🍝🚩 Checkpoint ./public/games/blackjack/blackjack.js:5646401/3713 ./public/games/blackjack/index.html:5646401/4837

--- public/games/blackjack/blackjack.js
+++ public/games/blackjack/blackjack.js
@@ -15,6 +15,15 @@ const values = [
   "K",
 ];
 
-var suit = Math.floor(Math.random() * suits.length);
-var value = Math.floor(Math.random() * values.length);
+function hitme() {
+  var suit = Math.floor(Math.random() * suits.length);
+  var value = Math.floor(Math.random() * values.length);
+  var card = suit ;
 
+  var log = document.createElement("txt");
+  log.txt = card;
+  log.id = card;
+  log.style = "color: red, font-size: 42px";
+  var LOG = document.getElementById("log").appendChild(log);
+  alert(log.txt);
+}
