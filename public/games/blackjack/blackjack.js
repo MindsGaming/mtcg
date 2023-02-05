ada74932bdd42ac616ba11415ac01f5b2a08f31d
Glitch (mtcg) <none>
2023-02-05 18:21:16 +0000

    🍧🎥 Checkpoint ./public/games/blackjack/index.html:5646401/2747 ./public/games/blackjack/blackjack.js:5646401/23105

--- public/games/blackjack/blackjack.js
+++ public/games/blackjack/blackjack.js
@@ -15,58 +15,6 @@ const values = [
   "K",
 ];
 
-getDeck();
+var suit = Math.floor(Math.random() * suits.length);
+var value = Math.floor(Math.random() * values.length);
 
-function getDeck() {
-  let deck = new Array();
-
-  for (let i = 0; i < suits.length; i++) {
-    for (let x = 0; x < values.length; x++) {
-      let card = { Value: values[x], Suit: suits[i] };
-      deck.push(card);
-    }
-  }
-
-  return deck;
-}
-
-const deck = [
-  { Value: "A", Suit: "Spades" },
-  { Value: "A", Suit: "Diamonds" },
-  { Value: "A", Suit: "Clubs" },
-];
-
-shuffle(deck);
-
-function shuffle(deck) {
-  // for 1000 turns
-  // switch the values of two random cards
-  for (let i = 0; i < 1000; i++) {
-    let location1 = Math.floor(Math.random() * deck.length);
-    let location2 = Math.floor(Math.random() * deck.length);
-    let tmp = deck[location1];
-
-    deck[location1] = deck[location2];
-    deck[location2] = tmp;
-  }
-}
-
-renderDeck(deck);
-function renderDeck(deck) {
-  document.getElementById("deck").innerHTML = "";
-
-  for (let i = 0; i < deck.length; i++) {
-    let card = document.createElement("div");
-    let value = document.createElement("div");
-    let suit = document.createElement("div");
-    card.className = "card";
-    value.className = "value";
-    suit.className = "suit " + deck[i].Suit;
-
-    value.innerHTML = deck[i].Value;
-    card.appendChild(value);
-    card.appendChild(suit);
-
-    document.getElementById("deck").appendChild(card);
-  }
-}
