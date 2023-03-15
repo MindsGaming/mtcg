bb3661785a2dcc6c01485ec3d925dd83238f581a
Glitch (mtcg) <none>
2023-03-15 01:01:20 +0000

    🎪🏘 Checkpoint ./public/chat/chat.js:5646401/4433

--- public/chat/chat.js
+++ public/chat/chat.js
@@ -19,19 +19,16 @@ function startCHAT() {
   } else {
     var currentrewards = document.getElementById("seconds");
     var currentrewardsM = document.getElementById("minutes");
-    
-    if (currentrewardsM.innerHTML > 0){
-    var saveM = currentrewardsM.innerHTML;
-    var totalrewards = currentrewardsM + "." + currentrewards;
-       var removerewards = 5;
+
+    if (currentrewardsM.innerHTML > 0) {
+      var saveM = currentrewardsM.innerHTML;
+      var totalrewards = currentrewardsM + "." + currentrewards;
+      var removerewards = 5;
       var removemath = removerewards - getcurrentrewards;
       reset();
       currentrewards.innerHTML = removemath;
       currentrewardsM.innerHTML = saveM;
-
-    
     }
-    
 
     var getcurrentrewards = currentrewards.innerHTML;
 
