59b10c85a786336284a7d9bff39ea06ca51afa4a
Glitch (mtcg) <none>
2023-03-15 00:14:20 +0000

    ☎️🎙 Checkpoint

--- public/chat/chat.js
+++ public/chat/chat.js
@@ -9,7 +9,13 @@ chatBOX.onkeydown = function (e) {
 };
 
 function startCHAT() {
+  var CHATBOX = document.getElementById("CHATBOX");
   var chaterror = document.getElementById("chaterror");
+  var STACKED = document.getElementById("STACKED");
+  var stacked = document.getElementById("stacked");
+  var currentM = document.getElementById("minutes");
+  var currentS = document.getElementById("seconds");
+  var rewardsmath = currentM.innerHTML + "." + currentS.innerHTML;
 
   if (dreamInput.value == "") {
     chatBOX.value = "";
@@ -17,55 +23,45 @@ function startCHAT() {
     chaterror.innerHTML = "No Account Found.";
     chatBOX.placeholder = "No Account Found.";
   } else {
-    var currentrewards = document.getElementById("seconds");
-    var currentrewardsM = document.getElementById("minutes");
-
-    if (currentrewardsM.innerHTML > 0) {
-      var saveM = currentrewardsM.innerHTML;
-      var totalrewards = currentrewardsM + "." + currentrewards;
-      var removerewards = 5;
-      var removemath = removerewards - totalrewards;
-      if (removemath > 0.99) {
-        currentrewards.innerHTML = removemath;
-        currentrewardsM.innerHTML = saveM;
-      }
-    } else {
-      var getcurrentrewards = currentrewards.innerHTML;
-
-      if (getcurrentrewards < 50) {
-        chatBOX.value = "";
-        chatBOX.focus;
-        var removerewards = 50;
-        var removemath = removerewards - getcurrentrewards;
-
-        chaterror.innerHTML = "." + removemath + "Rewards Needed.";
-        chatBOX.placeholder = "." + removemath + "Rewards Needed.";
-        chaterror.title = "." + removemath + "Rewards Needed.";
-      } else {
-        var removerewards = 5;
-        var removemath = removerewards - getcurrentrewards;
-        reset();
-        currentrewards.innerHTML = removemath;
-        currentrewardsM.innerHTML = saveM;
-
-        const data = {
-          dream: dreamInput.value + " Commented: " + chatBOX.value,
-        };
-        fetch("/addDream", {
-          method: "POST",
-          body: JSON.stringify(data),
-          headers: { "Content-Type": "application/json" },
-        })
-          .then((res) => res.json())
-          .then((response) => {
-            console.log(JSON.stringify(response));
-          });
-        // get dream value and add it to the list
-        dreams.push(dreamInput.value + " Commented: " + chatBOX.value);
-        appendNewDream(dreamInput.value + " Commented: " + chatBOX.value);
-      }
-      chatBOX.value = "";
-      chatBOX.focus;
+    if (STACKED.value > 0.5) {
+      var postcost = 0.5;
+      var postmath = postcost - STACKED.value;
+      var newstack = document.getElementById("STACKED");
+      newstack.value = postmath;
+      stacked.innerHTML = newstack.value;
     }
+  }
+  if (rewardsmath < 0.5) {
+    var postcost = 0.5;
+    var postmath = postcost - STACKED.value;
+    chaterror.innerHTML = postmath + "Rewards Needed.";
+    chatBOX.placeholder = postmath + "Rewards Needed";
+  } else {
+    var postcost = 0.5;
+    var postmath = rewardsmath + STACKED.value - postcost;
+    var newstack = document.getElementById("STACKED");
+    newstack.value = postmath;
+    stacked.innerHTML = newstack.value;
+
+    const data = {
+      dream: dreamInput.value + " Commented:" + CHATBOX.value,
+    };
+    fetch("/addDream", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        console.log(JSON.stringify(response));
+      });
+    // get dream value and add it to the list
+    dreams.push(dreamInput.value + " Commented:" + CHATBOX.value);
+    appendNewDream(dreamInput.value + " Commented:" + CHATBOX.value);
+    CHATBOX.innerHTML = "";
+    CHATBOX.focus;
+    chaterror.innerHTML = "";
+
+    reset();
   }
 }
