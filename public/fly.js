cf7e006f511b12b13c0835edc599b148c99e92fb
Glitch (mtcg) <none>
2023-03-18 17:33:05 +0000

    Rewound to commit 191ea0de031ea24b1f12f57a90f25f2ab0983d6b + 0 edits

--- public/fly.js
+++ public/fly.js
@@ -124,7 +124,8 @@ function quickhub() {
     games.className = "hide";
     games.title = "closed";
     var createHUB = document.createElement("iframe");
-    createHUB.src = "https://needle-castle.glitch.me/";
+    createHUB.src =
+      "https://hubs.mozilla.com/EJFaEcZ?embed_token=d4f6c2c54a1a684be3668b366724fb05";
     createHUB.className = "wrap";
     createHUB.allow = "microphone; camera; vr; speaker;";
     var CREATEHUB = document.getElementById("HUBS").appendChild(createHUB);
