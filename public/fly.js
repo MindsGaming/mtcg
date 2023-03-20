71ed7e887c1f08cf55e2df24e0fdadd8602e95d3
Glitch (mtcg) <none>
2023-03-20 17:44:42 +0000

    📙⛵️ Checkpoint ./views/index.html:5646401/67501 ./public/style.css:5646401/31967 ./public/fly.js:5646401/31209

--- public/fly.js
+++ public/fly.js
@@ -469,6 +469,29 @@ function Bsocial() {
   window.open("/chat", "_self");
 }
 
+/* J Social */
+function Jsocial() {
+  var SOCIAL = document.getElementById("SOCIAL");
+  if (SOCIAL.title == "CLOSED") {
+    SOCIAL.title == "JITSI";
+    var createHUB = document.createElement("iframe");
+    createHUB.src = "/chat/jitsi.html";
+    createHUB.style =
+      "position:fixed; top:0 left:0; width: 100%; height: 100%; background-color: white; z-index: 2 ";
+    SOCIAL.className = "";
+    SOCIAL.style =
+      "position:fixed; top:0 left:0; width: 100%; height: 100%; background-color: white; z-index: 2 ";
+    var CREATEHUB = document.getElementById("SOCIAL").appendChild(createHUB);
+    SOCIAL.className = "wrap";
+  } else {
+    if (SOCIAL.title == "JITSI") {
+      SOCIAL.title == "CLOSED";
+
+      SOCIAL.className = "hide";
+    }
+  }
+}
+
 /* Online Check */
 
 const status = window.navigator.onLine;
