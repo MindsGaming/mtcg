0bd2e5f7bf0e31f2437cbfd199ab7dab52b8ebb2
Glitch (mtcg) <none>
2024-01-25 15:24:28 +0000

    📙🌇 Checkpoint ./public/term/term.css:5646401/265 ./views/index.html:5646401/101 ./public/term/index.html:5646401/6725 ./public/term/term.js:5646401/747

--- public/term/term.js
+++ public/term/term.js
@@ -1,7 +1,11 @@
+function Rplaceholder() {
+  var drea
+  dreams.placeholder = "";
+}
+
 function commands() {
   var fetchcommand = dreamInput.value;
   dreamsList.innerHTML = "";
-
   if (fetchcommand == "help") {
     dreamInput.value =
       "Welcome To The Game Terminal. You Can Enter Commands Like: help";
