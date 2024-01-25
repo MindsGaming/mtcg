793178d70d7ca22eb05344a2180d8ed15a7d4aba
Glitch (mtcg) <none>
2024-01-25 17:17:43 +0000

    🎙🍁 Checkpoint ./public/term/index.html:5646401/46 ./public/term/term.js:5646401/8954 ./public/term/term.css:5646401/19

--- public/term/term.js
+++ public/term/term.js
@@ -9,23 +9,16 @@ function Rplaceholder() {
 function commands() {
   var fetchcommand = dreamInput.value;
   var fetchlogin = document.getElementById("fetchlogin");
-  var fetchcpureply = document.getElementById("fetch-cpu-reply");
   dreamsList.innerHTML = "";
   if (fetchcommand == "help") {
     dreamInput.value =
-      "Welcome To The Game Terminal. A Gamerfied Way To Earn Rewards, You Can Enter Commands Like: 'help', 'login',";
+      "Welcome To The Game Terminal. A Gamerfied Way To Earn Rewards, You Can Enter Commands Like: 'help', 'login','refresh'";
   }
   if (fetchcommand == "login") {
     dreamInput.value = "Enter Your Wallet Reciver Adderess...";
-  } else {
-    validateForm();
-    function validateForm() {
-      dreamInput.value = fetchcommand;
-      dreamInput.minlength = "42";
-      dreamInput.maxlength = "43";
-      appendNewDream;
-      fetchcpureply.className = "hide";
-    }
-    dreamsList.innerHTML = "";
+  }
+
+  if (fetchcommand == "refresh") {
+    dreamInput.value = "Enter Your Wallet Reciver Adderess...";
   }
 }
