28ceb39ebe909a995b64567dcdf9d8931136cc0d
Glitch (mtcg) <none>
2024-01-26 18:31:31 +0000

    ⛳️🍔 Checkpoint ./public/style.css:5646401/2 ./public/term/term.js:5646401/22186 ./public/term/index.html:5646401/3847

--- public/term/term.js
+++ public/term/term.js
@@ -10,60 +10,65 @@ function Rplaceholder() {
 
 function commands() {
   var fetchcommand = dreamInput.value;
+  var dreamsX = document.getElementById("dreamsX");
 
   if (fetchcommand == "hello") {
-    dreamInput.value = "I am not a chatbot, please enter a command.";
+    dreamsX.value = "I am not a chatbot, please enter a command.";
   } else {
     if (fetchcommand == "help") {
-      dreamInput.value =
+      dreamsX.value =
         "MTCG Game Terminal Is A Gamerfied Way To Earn Rewards! Commands: {'help', 'login','refresh','remix',...}";
     } else {
       if (fetchcommand == "login") {
-        dreamInput.value = "Enter Your Token Contact Adderess...";
+        dreamsX.value = "Enter Your Token Contact Adderess...";
       } else {
         if (fetchcommand == "refresh") {
-          dreamInput.value = "";
+          dreamsX.value.value = "";
           clearposts();
         }
       }
 
       if (fetchcommand == "rewards") {
-        dreamInput.value = "Coming Soon...";
+        dreamsX.value = "Coming Soon...";
       } else {
         if (fetchcommand == "remix") {
           var remix = document.getElementById("remix");
           remix.className = "remix";
           remix.src = "https://glitch.com/edit/#!/remix/mtcg";
-          dreamInput.value = remix.outterHTMl;
+          dreamsX.value = remix.outterHTMl;
           let fun = dreamInput.innerHTML;
           fun.innerHTML = "Your Remix";
         } else {
           if (fetchcommand == "0xDA7F833675D2F90E3Df02c36bad3ECc1417B93dD") {
-            dreamInput.value = "GAMER";
+            dreamsX.value = "GAMER";
           } else {
             if (fetchcommand == "0xCbc63Dcc51679aDf0394AB2be1318034193003B6") {
-              dreamInput.value = "ECLIPSE";
+              dreamsX.value = "ECLIPSE";
             } else {
               if (
                 fetchcommand == "0x936e08736F882144Efd53813Ee9805701A5f4dC3"
               ) {
-                dreamInput.value = "DooBetter";
+                dreamsX.value = "DooBetter";
               } else {
                 if (
                   fetchcommand == "0x7f0A21Dd46a87C3cEA64A36D771B13257b345324"
                 ) {
-                  dreamInput.value = "GTPC";
+                  dreamsX.value = "GTPC";
                 } else {
                   if (
                     fetchcommand == "0xD44bf50dc440a38bF76499cC854EACe4d6e350F4"
                   ) {
-                    dreamInput.value = "PINN";
+                    dreamsX.value = "PINN";
                   } else {
                     if (
                       fetchcommand ==
                       "0xD44bf50dc440a38bF76499cC854EACe4d6e350F4"
                     ) {
-                      dreamInput.value = "PINN";
+                      dreamsX.value = "PINN";
+                    } else {
+                      if (fetchcommand == "test") {
+                        dreamsX.value = "PINN";
+                      }
                     }
                   }
                 }
@@ -71,7 +76,7 @@ function commands() {
             }
 
             if (dreamInput.value == fetchcommand) {
-              dreamInput.value = "Command Not Found :(";
+              dreamsX.value = "Command Not Found :(";
             }
           }
         }
@@ -90,6 +95,7 @@ function clearposts() {
 }
 
 function viewLOGIN() {
+   var dreamsX = document.getElementById("dreamsX");
   if (infinity.title == "Infinity") {
     infinity.title = "Terminal";
     infinity.src = "/term/index.html";
@@ -97,8 +103,7 @@ function viewLOGIN() {
     splash.className = "splash";
     infinity.className = "apps";
     info.className = "hide";
-    clearposts();
-  } else {
+dreamsX.innerHTML = ""  } else {
     infinity.title = "Infinity";
     infinity.src =
       "https://opengpts-example-vz4y4ooboq-uc.a.run.app/?shared_id=f8e255b1-6ac6-4b5b-af4c-5a00afad18bc?shared_id=a27e9bc2-50ff-4106-bfcc-e609e0ca62b3";
