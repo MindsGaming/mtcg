0bb165917c2daa4d4e5d189016387ee6ab88eab1
Glitch (mtcg) <none>
2024-02-22 02:06:07 +0000

    📇👗 Checkpoint ./public/term/term.js:5646401/36966

--- public/term/term.js
+++ public/term/term.js
@@ -11,14 +11,13 @@ function Rplaceholder() {
 function commands() {
   var fetchcommand = dreamInput.value;
   var fetchlogin = document.getElementById("fetchlogin");
-  dreamsForm.pattern = "{4,100}";
 
   if (fetchcommand == "hello") {
     dreamInput.value = "I am not a chatbot, please enter a command.";
   } else {
     if (fetchcommand == "help") {
       dreamInput.value =
-        "MTCG Game Terminal Is A Gamerfied Way To Earn Rewards! Commands: {'help', 'login','refresh','remix',...}";
+        "MTCG Game Terminal Is A Gamerfied Way To Earn Rewards! Commands: {'help', 'login','refresh','remix','chess'..}";
     } else {
       if (fetchcommand == "login") {
         layout();
@@ -37,40 +36,12 @@ function commands() {
           remix.src = "https://glitch.com/edit/#!/remix/mtcg";
           dreamInput.value = remix.outterHTMl;
         } else {
-          if (fetchcommand == "0xDA7F833675D2F90E3Df02c36bad3ECc1417B93dD") {
-            dreamInput.value = "GAMER";
+          if (fetchcommand == "chess") {
+            window.open("https://testnet.chesspolygon.com/", "_blank");
+            dreamInput.value = "";
           } else {
-            if (fetchcommand == "0xCbc63Dcc51679aDf0394AB2be1318034193003B6") {
-              dreamInput.value = "ECLIPSE";
-            } else {
-              if (
-                fetchcommand == "0x936e08736F882144Efd53813Ee9805701A5f4dC3"
-              ) {
-                dreamInput.value = "DooBetter";
-              } else {
-                if (
-                  fetchcommand == "0x7f0A21Dd46a87C3cEA64A36D771B13257b345324"
-                ) {
-                  dreamInput.value = "GTPC";
-                } else {
-                  if (
-                    fetchcommand == "0xD44bf50dc440a38bF76499cC854EACe4d6e350F4"
-                  ) {
-                    dreamInput.value = "PINN";
-                  } else {
-                    if (
-                      fetchcommand ==
-                      "0xD44bf50dc440a38bF76499cC854EACe4d6e350F4"
-                    ) {
-                      dreamInput.value = "PINN";
-                    }
-                  }
-                }
-              }
-            }
-
             if (dreamInput.value == fetchcommand) {
-              dreamInput.value = "Command Logged;";
+              dreamInput.value = "Logged; " + fetchcommand;
             }
           }
         }
