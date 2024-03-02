ce4d3d50e16df323072a4dbf466800ca5730fa16
Glitch (mtcg) <none>
2024-03-02 20:03:46 +0000

    🎆📉 Checkpoint ./public/dwmw/dwmw.js:5646401/11842 ./public/dwmw/index.html:5646401/8551

--- public/dwmw/dwmw.js
+++ public/dwmw/dwmw.js
@@ -1,7 +1,36 @@
-const watch = document.getElementByID("watchDWMW")
-function watchDWMW(){
-  if (watch.title == "E1"){
-    
+const watch = document.getElementById("watchDWMW");
+function watchDWMW() {
+  if (watch.title == "E1") {
+    window.open("https://www.minds.com/embed/1510323962700107779", "_blank");
+    watch.title = "E2";
+    watch.innerHTML = "Eposide Two";
+  } else {
+    if (watch.title == "E2") {
+      window.open(
+        "https://gitlab.com/MindsGaming/dwmw/-/raw/main/Full%20Eposides/DWMW_ep_001_2_.mp4?ref_type=heads",
+        "_blank"
+      );
+      watch.title = "E3";
+      watch.innerHTML = "Eposide Three";
+    } else {
+      if (watch.title == "E3") {
+        watch.innerHTML = "Coming Soon...";
+      }
+    }
   }
-  
+}
+
+function DWMWdonate() {
+  window.open("https://www.mintme.com/token/DWMW", "_blank");
+}
+
+function joinshow() {
+  window.open(
+    "https://www.minds.com/group/1501695298248904715/latest",
+    "_blank"
+  );
+}
+
+function DWMWgit() {
+  window.open("https://gitlab.com/MindsGaming/dwmw", "_blank");
 }
