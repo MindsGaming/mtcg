f8fc818d01a6fd27462ac59d035fdfb52a58ccb2
Glitch (mtcg) <none>
2024-03-19 21:41:48 +0000

    🚤📌 Checkpoint ./public/client.js:5646401/45481

--- public/client.js
+++ public/client.js
@@ -290,7 +290,7 @@ function checkPage() {
 
 function thefarmers() {
   let checkpoints = parseInt(POINTS.innerHTML);
-  if (checkpoints > 2000) {
+  if (checkpoints > 100) {
     gamerpointsBTN.className = "";
   }
 }
@@ -302,17 +302,9 @@ function GamerPOINTS() {
   const EGGmath = Math.round(checkPOINTS - removePOINTS);
   POINTS.innerHTML = EGGmath;
 
-  if (POINTS.innerHTML < 2000) {
+  if (POINTS.innerHTML < 100) {
     gamerpointsBTN.className = "hide";
   } else {
-    const dreams = [];
-
-    // define variables that reference elements on our page
-    const dreamsForm = document.forms[0];
-    const dreamInput = dreamsForm.elements["dream"];
-    const dreamsList = document.getElementById("dreams");
-    const clearButton = document.querySelector("#clear-dreams");
-
     // request the dreams from our app's sqlite database
     fetch("/getDreams", {})
       .then((res) => res.json())
@@ -325,7 +317,12 @@ function GamerPOINTS() {
     // a helper function that creates a list item for a given dream
     const appendNewDream = (dream) => {
       const newListItem = document.createElement("li");
-      newListItem.innerText = dream;
+      const randomWarp = Math.floor(Math.random() * numb) + 1;
+      newListItem.title = "DreamEGG";
+      newListItem.id = numb;
+      newListItem.value = randomWarp;
+      newListItem.className = "";
+      newListItem.innerText = "TEST";
       dreamsList.appendChild(newListItem);
     };
 
@@ -336,22 +333,9 @@ function GamerPOINTS() {
 
       const data = { dream: dreamInput.value + " :5 GAMER" };
 
-      fetch("/addDream", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: { "Content-Type": "application/json" },
-      })
-        .then((res) => res.json())
-        .then((response) => {
-          console.log(JSON.stringify(response));
-        });
       // get dream value and add it to the list
-      dreams.push(dreamInput.value);
-      appendNewDream(dreamInput.value);
-
-      // reset form
-      dreamInput.value = "";
-      dreamInput.focus();
+      dreams.push(dreamInput.value + ":5 GAMER");
+      appendNewDream(dreamInput.value + ":5 GAMER");
     };
   }
 }
