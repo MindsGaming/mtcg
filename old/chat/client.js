c28328ddd6a2a757fb52738fef1f213cab03431c
Glitch (mtcg) <none>
2024-05-05 19:23:12 +0000

    Rewound to commit dec6cf912b61b9c0c3364667f2f37467608855d8 + 0 edits

---
+++ old/chat/client.js
@@ -0,0 +1,97 @@
+// run by the browser each time your view template referencing it is loaded
+
+console.log("hello world :o");
+
+const dreams = [];
+
+// define variables that reference elements on our page
+const dreamsForm = document.forms[0];
+const dreamInput = dreamsForm.elements["dream"];
+const dreamsList = document.getElementById("dreams");
+const clearButton = document.querySelector("#clear-dreams");
+const myUSER = document.getElementById("myUSER");
+
+// request the dreams from our app's sqlite database
+fetch("/getDreams", {})
+  .then((res) => res.json())
+  .then((response) => {
+    response.forEach((row) => {
+      appendNewDream(row.dream);
+    });
+  });
+
+// a helper function that creates a list item for a given dream
+const appendNewDream = (dream) => {
+  const newListItem = document.createElement("li");
+  newListItem.className = "POST";
+  newListItem.innerText = dream;
+
+  var totalposts = document.getElementById("totalPOSTS");
+  var addpost = 1;
+  var currentposts = totalposts.value;
+  var gettotalposts = totalposts.value + addpost;
+  totalposts.value = gettotalposts;
+  newListItem.id = gettotalposts;
+
+  var last = document.getElementById("last");
+  last.className = "POST";
+  last.innerHTML = "";
+  if (last.title < gettotalposts) {
+    last.innerHTML = dream;
+  }
+
+  dreamsList.appendChild(newListItem);
+};
+
+// listen for the form to be submitted and add a new dream when it is
+dreamsForm.onsubmit = (event) => {
+  // stop our form submission from refreshing the page
+  event.preventDefault();
+
+  if (ethereum.selectedAddress == null) {
+    var POP = document.getElementById("connect");
+    POP.className = "POP";
+    POP.innerHTML = "Click To Connect Blockchain";
+  } else {
+    myUSER.innerHTML = ethereum.selectedAddress;
+
+    const data = {
+      dream: myUSER.innerHTML + " Said: " + dreamInput.value,
+    };
+
+    fetch("/addDream", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        console.log(JSON.stringify(response));
+      });
+    // get dream value and add it to the list
+    dreams.push(myUSER.innerHTML + " Said:" + dreamInput.value);
+    appendNewDream(myUSER.innerHTML + " Said: " + dreamInput.value);
+
+    // reset form
+    dreamInput.value = "";
+    dreamInput.focus();
+    closePOP();
+  }
+
+  clearButton.addEventListener("click", (event) => {
+    fetch("/clearDreams", {})
+      .then((res) => res.json())
+      .then((response) => {
+        console.log("cleared dreams");
+      });
+    dreamsList.innerHTML = "";
+
+    var lastpost = document.getElementById("last");
+    lastpost.className = "new";
+    lastpost.innerHTML = "Create  A Post";
+  });
+};
+
+function GOHOME() {
+  window.open("/", "_self");
+}
---
+++ old/chat/swap.js
@@ -0,0 +1,104 @@
+const swapBTN = document.getElementById("swapBTN");
+
+function swaping() {
+  var mytoken = document.getElementById("myToken");
+  var swapone = document.getElementById("swapONE");
+  var swaptwo = document.getElementById("swapTWO");
+  var swapmap = document.getElementById("swapmap");
+  swapmap.className = "swapmap";
+  swapone.innerHTML = mytoken.innerHTML;
+
+  if (swaptwo.innerHTML == "DarkMark") {
+    swaptwo.innerHTML = "DragonToken";
+  } else {
+    if (swaptwo.innerHTML == "DragonToken") {
+      swaptwo.innerHTML = "Candys";
+    } else {
+      if (swaptwo.innerHTML == "Candys") {
+        swaptwo.innerHTML = "PINN";
+      } else {
+        if (swaptwo.innerHTML == "PINN") {
+          swaptwo.innerHTML = "withinthevacuum";
+        } else {
+          if (swaptwo.innerHTML == "withinthevacuum") {
+            swaptwo.innerHTML = "ECLIPSE";
+          } else {
+            if (swaptwo.innerHTML == "ECLIPSE") {
+              swaptwo.innerHTML = "DooBetter";
+            } else {
+              if (swaptwo.innerHTML == "DooBetter") {
+                swaptwo.innerHTML = "GTPC";
+              } else {
+                if (swaptwo.innerHTML == "GTPC") {
+                  swaptwo.innerHTML = "GAMER";
+                } else {
+                  if (swaptwo.innerHTML == "GAMER") {
+                    swaptwo.innerHTML = "DarkMark";
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  swapBTN.innerHTML = "Swap Ready!";
+}
+
+function swapready() {
+  var mytoken = document.getElementById("myToken");
+  var swapone = document.getElementById("swapONE");
+  var swaptwo = document.getElementById("swapTWO");
+  var swapmap = document.getElementById("swapmap");
+  var swaperror = document.getElementById("swaperror");
+  var currentrewardsM = document.getElementById("minutes");
+  var currentrewards = document.getElementById("seconds");
+  var gettotal = currentrewardsM.innerHTML + "." + currentrewards.innerHTML;
+  var stashwallet = swapone.innerHTML + "STASH";
+  var swapwallet = swaptwo.innerHTML + "STASH";
+  var stashid = document.getElementById(stashwallet);
+  var stashswap = document.getElementById(swapwallet);
+  var SWAPMATH = document.getElementById("SWAPMATH");
+  var SWAPMATHTWO = document.getElementById("SWAPMATHTWO");
+  var SWAPTIMER = document.getElementById("SWAPTIMER");
+  SWAPTIMER.value = gettotal;
+  if (swapone.innerHTML == swaptwo.innerHTML) {
+    swaping();
+  } else {
+    swapone.innerHTML = mytoken.innerHTML;
+    if (swapone.innerHTML == swaptwo.innerHTML) {
+      swaperror.innerHTML = "Can't Swap The Same Reward.";
+    } else {
+      if (stashid.innerHTML > 1) {
+        var fetchTIMER = document.getElementById("SWAPTIMER");
+        var payswap = 1;
+        var fishswap = SWAPMATHTWO.value;
+        var payedswap = fishswap - payswap;
+        SWAPMATH.value = payedswap;
+        var fishtotal = fetchTIMER.value + SWAPMATHTWO.value;
+        stashid.innerHTML = "0";
+        stashswap.innerHTML = fishtotal;
+        reset();
+      } else {
+        if (gettotal < 1) {
+          var needmath = 1 - gettotal;
+          swaperror.innerHTML = "Earn +" + needmath + "Rewards.";
+        } else {
+          if (gettotal > 1) {
+            var fetchTIMER = document.getElementById("SWAPTIMER");
+
+            var payswap = 1;
+            var fishswap = SWAPMATH.value;
+            var payedswap = fetchTIMER.value - payswap;
+            SWAPMATHTWO.value = payedswap;
+            var fishtotal = fishswap + SWAPMATHTWO.value;
+            stashid.innerHTML = "0";
+            stashswap.innerHTML = fishtotal;
+            reset();
+          }
+        }
+      }
+    }
+  }
+}
