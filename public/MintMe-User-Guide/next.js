617fbb8a84c57f02038d39185523bca2f98434e5
Glitch (mtcg) <none>
2024-08-25 18:28:39 +0000

    🕴👌 Checkpoint ./public/MintMe-User-Guide/style.css:5646401/9162 ./public/MintMe-User-Guide/index.html:5646401/183 ./public/MintMe-User-Guide/next.js:5646401/2380

---
+++ public/MintMe-User-Guide/next.js
@@ -0,0 +1,89 @@
+var next = document.getElementById("next");
+var nextBTN = document.getElementById("nextBTN");
+var bonusTab = document.getElementById("bonusTab");
+
+function nextVideo() {
+  if (next.title == "One") {
+    next.src = "https://www.youtube.com/embed/CiIRz7JGKcE?si=Q6b0aTS-0kc3Luh9";
+    next.title = "Two";
+    delayBTN();
+  } else {
+    if (next.title == "Two") {
+      next.src =
+        "https://www.youtube.com/embed/yBu_Bmvqp8M?si=pUGqMGRv1qh9I935";
+      next.title = "Three";
+      delayBTN();
+    } else {
+      if (next.title == "Three") {
+        next.src =
+          "https://www.youtube.com/embed/szNGaEEICLM?si=bQWIfWMVy6xM9vBR";
+        next.title = "Four";
+        delayBTN();
+      } else {
+        if (next.title == "Four") {
+          next.src =
+            "https://www.youtube.com/embed/zy0LF44jbkE?si=2yQdm1-DGJY4fwVO";
+          next.title = "Five";
+          delayBTN();
+        } else {
+          if (next.title == "Five") {
+            next.src =
+              "https://www.youtube.com/embed/38ncPWdbsYs?si=e3wMjnfttgI7CEj2";
+
+            next.title = "Six";
+            delayBTN();
+          } else {
+            if (next.title == "Six") {
+              next.src =
+                "https://www.youtube.com/embed/4D5oNiqYnbQ?si=jmRcBuLKuFEH2Jjs";
+              next.title = "Six";
+              let nextBTN = document.getElementById("nextBTN");
+              nextBTN.className = "hide";
+
+              if (bonusTab.className == "") {
+                let form = document.getElementById("form");
+                form.className = "";
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+function delayBTN() {
+  let timer = setTimeout(delay, 1500);
+  nextBTN.className = "hide";
+  function delay() {
+    nextBTN.className = "next";
+  }
+}
+
+function getGamerSponsor() {
+  let sponsor = document.getElementById("sponsor");
+  sponsor.title = "gamer";
+  bonusTab.className = "";
+  rollingOut();
+}
+
+function getBobSponsor() {
+  let sponsor = document.getElementById("sponsor");
+  sponsor.title = "bob";
+  bonusTab.className = "";
+  rollingOut();
+}
+
+function getDwmwSponsor() {
+  let sponsor = document.getElementById("sponsor");
+  sponsor.title = "dwmw";
+  bonusTab.className = "";
+  rollingOut();
+}
+
+/* HTTPS */
+
+var loc = window.location.href + "";
+if (loc.indexOf("http://") == 0) {
+  window.location.href = loc.replace("http://", "https://");
+}
