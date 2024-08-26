c1045c890fb082c7bbf8aad9bd3c0cca35bff86c
Glitch (mtcg) <none>
2024-08-26 19:41:40 +0000

    🍇🌮 Checkpoint ./public/MintMe-User-Guide/index.html:5646401/6664 ./public/MintMe-User-Guide/sponsor.js:5646401/1477

--- public/MintMe-User-Guide/sponsor.js
+++ public/MintMe-User-Guide/sponsor.js
@@ -12,49 +12,17 @@ function rollingOut() {
   if (sponsored.title == "Open") {
     pushAlert.innerHTMl = "Sponsored Haulted.";
   } else {
-    let floating = Math.floor(Math.random() * 25);
-    let blockTester = Math.floor(Math.random() * 25);
+  
+  var fetchSponsor = sponsored.title;
+    
+  let picker = ["0", "1", "2"];
+    
+let fetchPick = Math.floor(Math.random() * 3);
 
-    let hardness = blockTester;
-
-    if (blockTester == floating) {
-      let mySpins = ["0", "1", "2"];
-      let mySpin = "Open";
-      let checkSpin = Math.floor(Math.random() * 3);
-
-      if (checkSpin == 0) {
-        mySpin = "gamer";
-      } else {
-        if (checkSpin == 1) {
-          mySpin = "bob";
-        } else {
-          if (checkSpin == 2) {
-            mySpin = "dwmw";
-          }
-        }
-      }
-      if (sponsor.title == mySpin) {
-        let current = rollReward + 1;
-        let mathRoll = current;
-        rollReward = mathRoll;
-        sponsor.innerHTML = rollReward;
-        alert(rollReward);
-      }
-
-      rollingOut();
-    } else {
-      let newAttempt = attempts + 1;
-      let attemptMath = newAttempt;
-      attempts = attemptMath;
-      tracker.innerHTML = attemptMath;
-      let rollTimer = setTimeout(delay, setdelay);
-      function delay() {
-        if (setdelay == 1 || setdelay > 1) {
-          let delaymath = setdelay + 25;
-          setdelay = delaymath;
-        }
-        rollingOut();
-      }
+    if(fetchPick == 1){
+      
     }
-  }
+    
+    
+}
 }
